56981d8eff91c092034f0236da2fe9690b64d85c
remiboissise <remiboissise@gmail.com>
2015-02-25 17:43:29 +0000

    test11

--- inscription.php
+++ inscription.php
@@ -16,68 +16,10 @@ and open the template in the editor.
     <!-- FIN DESCRIPTION PAGE -->
     <!-- SCRIPT -->
 
-    <!--script language="Javascript" src="js/verifInscription.js"></script -->
-    <script>
-/*
-	Fonction permettant de surligner en rouge ou vert le champ s'il est bien remplit ou non
-*/
-
-function surligne(champ, erreur){
-   if(erreur)
-      champ.style.backgroundColor = "#fba";
-   else
-      champ.style.backgroundColor = "#54F98D";
-}
-
-/*
-	Fonction permettant de vérifier le champ correspondant au pseudo.
-*/
-
-function verifPseudo(champ){
-   if(champ.value.length < 2 || champ.value.length >= 20){ //Il ne peut pas faire moins de 2 caractère et plus de 20 caractères
-      surligne(champ, true);
-      return false;
-   }else{
-      surligne(champ, false);
-      return true;
-   }
-}
-
-/*
-	Fonction permettant de vérifier si le formulaire est complet. S'il ne l'est pas alors on aura une alert pop-up.
-*/
-
-function verifForm(f){
-   var pseudoOK = verifPseudo(f.email);
-   if(pseudoOK)
-      return true;
-   else{
-      alert("Les champs surligne en rouge doivent etre remplie correctement !!!");
-      return false;
-   }
-}
-
-/*
-	Cette fonction va nous permettre de passer au champ suivant lorsque la taille maximale est atteinte
-*/
-
-function suivantLimite(enCours, suivant, limite) 
-  { 
-  if (enCours.value.length == limite){
-    document.forms["fInscription"].elements[suivant].focus();
-  }
-}
-
-/*
-	Cette fonction va nous permettre de passer au champ suivant si l'on clique sur la touche ENTER 
-*/
-
-function suivantEnter(event, suivant){
-	if(event.which ==13){
-		document.forms["fInscription"].elements[suivant].focus();
-	}
-  </script>
+  <script language="Javascript" src="../js/verifInscription.js"></script>
+    
     <!-- FIN SCRIPT-->
+    
     <!-- DEBUT PAGE -->
     <body>
         <div class="content">
@@ -98,15 +40,14 @@ function suivantEnter(event, suivant){
 
                     Vérification Mot de passe : <input type="password" maxlength="20" name="case3"><br/>
                     
-                   <input button="submit" value="valider"/>
-
-                    Vous êtes :<Select name"Fonction">
+                  Vous êtes :<Select name"Fonction">
                   <option>------</option>
                   <option>Artiste</option>
                   <option>Gérant</option>
                   <option>Utilisateur</option>    
                   </select>
           
+          <input button="submit" value="valider">
           </form>
             </section>
 
