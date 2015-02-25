7dc3875f150c86330a3ec582181f6943aeaad9e6
remiboissise@gmail.com <remiboissise@gmail.com@df39fe93-acf2-2b65-9ade-a2c068ddeaf1>
2015-02-25 13:34:48 +0000

    Test1

---
+++ trunk/include/foot.php
@@ -0,0 +1,10 @@
+<footer>
+  <p>Publié par l'équipe de L3-MIAGE Sorbonne composé de :</p>
+    <ul>
+      <li> Jihane Jomni </li>
+      <li> Rémi Boissise </li>
+      <li> Simon Zerdoun </li>
+    </ul>
+  <p> Nous contacter : <a href="mailto:l3équipeMatchup@sorbonne.com">l3équipeMatchup@sorbonne.com</a></p>
+</footer>
+    
