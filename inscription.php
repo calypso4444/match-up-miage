f6a3ba157f29dc9800339993aef739d07e65006a
lajungglespeedienne <lajungglespeedienne@gmail.com>
2015-02-25 19:19:28 +0000

    test

--- inscription.php
+++ inscription.php
@@ -6,9 +6,15 @@ and open the template in the editor.
 -->
 <html>
 
+    <!-- CONNEXION A LA BDD -->
+    <?php
+    $link = mysqli_connect("localhost", "root", "");
+    mysqli_select_db($link, "mu_db");
+    ?>
+
     <!-- DEBUT DESCRIPTION PAGE -->
     <head>
-        <title>MATCH'UP_ACCUEIL</title>
+        <title>MATCH'UP_INSCRIPTION</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="style/style.css" media="screen" />
@@ -32,30 +38,93 @@ and open the template in the editor.
             <?php include_once("include/menu.php") ?>
 
             <section>
-                <form name="fInscription">
-                
-                    Pseudo : <input type="text" maxlength="30" placeholder="lhommedu13100" name="case1" onKeyUp="suivantLimite(this, 'case2', 30)" onkeypress="suivantEnter(event, 'case2')" onblur="verifPseudo(this)"><br/>
+                <form method="post">
+                    <label>Pseudo: <input type="text" name="pseudo"/></label><br/>
+                    <label>Mot de passe: <input type="password" name="passe"/></label><br/>
+                    <label>Confirmation du mot de passe: <input type="password" name="passe2"/></label><br/>
+                    <label>Adresse e-mail: <input type="text" name="email"/></label><br/>
+                    <input type="submit" value="M'inscrire"/>
+                </form>
 
-                    Mot de passe : <input type="password" maxlength="20" name="case2" onKeyUp="suivantLimite(this, 'case3', 30)" onkeypress="suivantEnter(event, 'case3')"><br/>
+                <?php
+                //on verifie d'abord que les champs ne sont pas vides
+                if (empty($_POST['pseudo'])) {
+                    //on encadre l'emplacement en rouge avec un message
+                }
 
-                    Vérification Mot de passe : <input type="password" maxlength="20" name="case3"><br/>
-                    
-                  Vous êtes :<Select name"Fonction">
-                  <option>------</option>
-                  <option>Artiste</option>
-                  <option>Gérant</option>
-                  <option>Utilisateur</option>    
-                  </select>
-          
-          <input button="submit" value="valider">
-          </form>
+                if (empty($_POST['passe'])) {
+                    //on encadre l'emplacement en rouge avec un message
+                }
+
+                if (!empty($_POST['passe2'])) {
+                    //on encadre l'emplacement en rouge avec un message
+                }
+
+                if (!empty($_POST['email'])) {
+                    //on encadre l'emplacement en rouge avec un message
+                }
+
+                //on verifie que l'adresse mail a un format valide
+                if (!filter_var('email', FILTER_VALIDATE_EMAIL)) {
+                    //on encadre l'emplacement en rouge avec un message
+                }
+
+                if ((!empty($_POST['pseudo']))and ( !empty($_POST['email'])) and ( !empty($_POST['passe']))) {
+                    // Je mets aussi certaines sécurités ici…
+                    $passe = mysqli_real_escape_string($link, htmlspecialchars($_POST['passe']));
+                    $passe2 = mysqli_real_escape_string($link, htmlspecialchars($_POST['passe2']));
+                    if ($passe == $passe2) {
+                        $pseudo = mysqli_real_escape_string($link, htmlspecialchars($_POST['pseudo']));
+                        $email = mysqli_real_escape_string($link, htmlspecialchars($_POST['email']));
+                        // Je vais crypter le mot de passe.
+                        $passe = sha1($passe);
+
+                        mysqli_query($link, "INSERT INTO validation VALUES('', '$pseudo', '$passe', '$email')");
+                    } else {
+                        echo 'Les deux mots de passe que vous avez rentrés ne correspondent pas…';
+                    }
+                }
+                ?>
+
+                <?php
+                $quete = mysqli_query($link,"SELECT * FROM validation");
+                while ($validation = mysqli_fetch_array($quete)) {
+                    mail('j.jomni@hotmail.fr','test','<a href="validation.php?action=accepter&id=' . $validation['id'] . '">Accepter</a></br><a href="validation.php?action=refuser&id=' . $validation['id'] . '">Refuser</a>');
+//                    echo 'Pseudo: ';
+//                    echo $validation['pseudo'];
+//                    echo ' Mot de passe: ';
+//                    echo $validation['passe'];
+//                    echo ' E-mail: ';
+//                    echo $validation['email'];
+//                    echo '<a href="validation.php?action=accepter&id=' . $validation['id'] . '">Accepter</a>';
+//                    echo '<a href="validation.php?action=refuser&id=' . $validation['id'] . '">Refuser</a>';
+//                    echo '<br/>';
+                }
+
+                if (isset($_GET['action']) AND isset($_GET['id'])) {
+                    $action = $_GET['action'];
+                    if ($action == "accepter") {
+                        $id = $_GET['id'];
+                        $quete2 = mysql_query("SELECT * FROM validation WHERE id='$id'");
+                        $connexion = mysql_fetch_array($quete2);
+                        $pseudo = $connexion['pseudo'];
+                        $passe = $connexion['passe'];
+                        $email = $connexion['email'];
+                        mysql_query("INSERT INTO connexion VALUES('$id', '$pseudo', '$passe', '$email')");
+                        mysql_query("DELETE FROM validation WHERE id='$id'");
+                    } elseif ($action == "refuser") {
+                        $id = $_GET['id'];
+                        mysql_query("DELETE FROM validation WHERE id='$id'");
+                    }
+                }
+                ?>
             </section>
 
             <!-- FIN CONTENT-->
         </div>
 
         <!--DEBUT FOOTER-->
-        <?php include_once("include/foot.php") ?>
+<?php include_once("include/foot.php") ?>
 
         <!--FIN FOOTER-->
     </body>
