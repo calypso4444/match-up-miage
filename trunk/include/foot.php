35bcb052f0708a822b2bd2503893467cb647bf13
lajungglespeedienne@gmail.com <lajungglespeedienne@gmail.com@df39fe93-acf2-2b65-9ade-a2c068ddeaf1>
2015-02-25 13:54:19 +0000

    ffh

--- trunk/include/foot.php
+++ trunk/include/foot.php
@@ -1,13 +1,5 @@
-<<<<<<< .mine<footer>
-    <p>Publi&eacute; par l'&eacute;quipe de L3-MIAGE Sorbonne compos&eacute; de :</p>
-    <ul>
-      <li> Jihane Jomni </li>
-      <li> R&eacute;mi Boissise </li>
-      <li> Simon Zerdoun </li>
-    </ul>
-  <p> Nous contacter : <a href="mailto:l3&eacute;quipeMatchup@sorbonne.com">l3&eacute;quipeMatchup@sorbonne.com</a></p>
-</footer>=======<footer>
-  <p>Publi&eacute; par l'&eacute;quipe de L3-MIAGE Sorbonne compos&eacute; de :</p>
+<footer>
+  <p>Publi&eacute; par l'&eacute;quipe de L3-MIAGE Sorbonne compos&eacute;e de :</p>
     <ul>
       <li> Jihane Jomni </li>
       <li> R&eacute;mi Boissise </li>
@@ -15,4 +7,4 @@
     </ul>
   <p> Nous contacter : <a href="mailto:l3&eacute;quipeMatchup@sorbonne.com">l3&eacute;quipeMatchup@sorbonne.com</a></p>
 </footer>
-    >>>>>>> .theirs
+    
