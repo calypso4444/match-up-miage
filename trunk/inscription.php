98dea45400e8d9c97b7041d4877bf03ecd811b4c
remiboissise@gmail.com <remiboissise@gmail.com@df39fe93-acf2-2b65-9ade-a2c068ddeaf1>
2015-02-26 14:36:10 +0000

    js

--- trunk/inscription.php
+++ trunk/inscription.php
@@ -24,7 +24,36 @@ and open the template in the editor.
     
     <!-- SCRIPT -->
 
-	<script type="text/javascript" src="js/verificationInscription1"></script>
+	<script>
+			function surligne(champ, erreur){
+	if(erreur)
+      champ.style.backgroundColor = "#fba";
+	else
+      champ.style.backgroundColor = "#54F98D";
+	}
+
+	function verifPseudo(champ){
+		if(champ.value.length < 2 || champ.value.length >= 20){
+			surligne(champ, true);
+			return false;
+   		}else{
+      surligne(champ, false);
+      return true;
+   		}
+	}
+
+	function verifMail(champ){
+	var regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+		if(!regex.test(champ.value)){
+			surligne(champ, true);
+			alert("Attention votre adresse mail n'est pas valide");
+			return false;
+   		}else{
+   			surligne(champ, false);
+   			return true;
+   		}
+	}	
+	</script>
 
     <!-- FIN SCRIPT-->
 
