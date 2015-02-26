72c4b79815f631f748809851c1d52280020075fd
remiboissise <remiboissise@gmail.com>
2015-02-26 13:31:48 +0000

    test

--- inscription.php
+++ inscription.php
@@ -39,10 +39,10 @@ and open the template in the editor.
 
             <section>
                 <form method="post">
+	                <label>Adresse e-mail: <input type="text" name="email"/></label><br/>
                     <label>Pseudo: <input type="text" name="pseudo"/></label><br/>
                     <label>Mot de passe: <input type="password" name="passe"/></label><br/>
                     <label>Confirmation du mot de passe: <input type="password" name="passe2"/></label><br/>
-                    <label>Adresse e-mail: <input type="text" name="email"/></label><br/>
                     <input type="submit" value="M'inscrire"/>
                 </form>
             </section>
