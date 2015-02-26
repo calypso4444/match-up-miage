8e69600fcf4328f1791e4b98db9a37b5b8f58dc3
remiboissise <remiboissise@gmail.com>
2015-02-26 09:47:13 +0000

    php

--- inscription.php
+++ inscription.php
@@ -86,40 +86,6 @@ and open the template in the editor.
                 }
                 ?>
 
-                <?php
-                $quete = mysqli_query($link, "SELECT * FROM validation");
-                while ($validation = mysqli_fetch_array($quete)) {
-                    mail('j.jomni@hotmail.fr', 'test', '<a href="validation.php?action=accepter&id=' . $validation['id'] . '">Accepter</a></br><a href="validation.php?action=refuser&id=' . $validation['id'] . '">Refuser</a>');
-                    echo 'Pseudo: ';
-                    echo $validation['pseudo'];
-                    echo ' Mot de passe: ';
-                    echo $validation['passe'];
-                    echo ' E-mail: ';
-                    echo $validation['email'];
-                    echo '<a href="validation.php?action=accepter&id=' . $validation['id'] . '">Accepter</a>';
-                    echo '<a href="validation.php?action=refuser&id=' . $validation['id'] . '">Refuser</a>';
-                    echo '<br/>';
-                }
-
-                if (isset($_GET['action']) AND isset($_GET['id'])) {
-                    $action = $_GET['action'];
-                    if ($action == "accepter") {
-                        $id = $_GET['id'];
-                        $quete2 = mysql_query("SELECT * FROM validation WHERE id='$id'");
-                        $connexion = mysql_fetch_array($quete2);
-                        $pseudo = $connexion['pseudo'];
-                        $passe = $connexion['passe'];
-                        $email = $connexion['email'];
-                        mysql_query("INSERT INTO connexion VALUES('$id', '$pseudo', '$passe', '$email')");
-                        mysql_query("DELETE FROM validation WHERE id='$id'");
-                    } elseif ($action == "refuser") {
-                        $id = $_GET['id'];
-                        mysql_query("DELETE FROM validation WHERE id='$id'");
-                    }
-                }
-                ?>
-
-
             <!-- FIN CONTENT-->
         </div>
 
