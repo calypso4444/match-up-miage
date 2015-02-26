4afad5ffc1f5d3e437fdd2f9ec33742a87b0c3f4
remiboissise <remiboissise@gmail.com>
2015-02-26 13:44:50 +0000

    test

--- js/verificationInscription1.js
+++ js/verificationInscription1.js
@@ -14,33 +14,3 @@ function verifPseudo(champ){
       return true;
    }
 }
-
-function verifForm(f){
-   var pseudoOK = verifPseudo(f.email);
-   if(pseudoOK)
-      return true;
-   else{
-      alert("Les champs surligne en rouge doivent etre remplie correctement !!!");
-      return false;
-   }
-}
-
-/*
-	Cette fonction va nous permettre de passer au champ suivant lorsque la taille maximale est atteinte
-*/
-
-function suivantLimite(enCours, suivant, limite) 
-  { 
-  if (enCours.value.length == limite){
-    document.forms["verificationInscription1"].elements[suivant].focus();
-  }
-}
-
-/*
-	Cette fonction va nous permettre de passer au champ suivant si l'on clique sur la touche ENTER 
-*/
-
-function suivantEnter(event, suivant){
-	if(event.which == 13){
-		document.forms["verificationInscription1"].elements[suivant].focus();
-	}
