2042314b690d123116a7a31f52340dd66583217e
simon.zerdoun <simon.zerdoun@gmail.com>
2015-02-26 10:58:21 +0000

    a propos du site

---
+++ a_propos_du_site.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+
+    <!-- DEBUT DESCRIPTION PAGE -->
+    <head>
+        <title>MATCH'UP_A_PROPOS_DU_SITE</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style/style.css" media="screen" />
+    </head>
+    <!-- FIN DESCRIPTION PAGE -->
+
+    <!-- DEBUT PAGE -->
+    <body>
+        <div class="content">
+            <!-- DEBUT HEADER -->
+            <?php include_once("include/head.php") ?>
+            <!-- FIN HEADER -->
+
+            <!-- DEBUT CONTENT -->
+            <?php include_once("include/menu.php") ?>
+        <!-- FIN CONTENT-->
+        
+          <p>
+            Notre but premier est de lancer des artistes sur le devant de la scène. </br>            
+            Par conséquent, nous avons décidé d’entreprendre nos travaux sur une refonte d’un site
+            exclusivement dédié aux artistes musicaux encore inconnu mais pouvant très certainement
+            cacher des talents inimaginables.
+          </p>
+          <p>  
+            En effet grâce à notre site, les jeunes prodiges pourront partager leur musique, commenter
+            la musique de leur confère, organiser et partager des événements avec leurs fans.                                              
+          </p>
+          
+        </div>
+
+        <!--DEBUT FOOTER-->
+        <?php include_once("include/foot.php") ?>
+
+        <!--FIN FOOTER-->
+    </body>
+</html>
