251b1b45be0785822ea2acfb7e68a6dad9c10b25
remiboissise@gmail.com <remiboissise@gmail.com@df39fe93-acf2-2b65-9ade-a2c068ddeaf1>
2015-02-26 13:49:08 +0000

    test

--- trunk/js/verificationInscription1.js
+++ trunk/js/verificationInscription1.js
@@ -14,3 +14,26 @@ function verifPseudo(champ){
       return true;
    }
 }
+
+function verifMail(champ){
+   var regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+   if(!regex.test(champ.value)){
+      surligne(champ, true);
+      return false;
+   }else{
+      surligne(champ, false);
+      return true;
+   }
+}
+
+function suivantLimite(enCours, suivant, limite) 
+  { 
+  if (enCours.value.length == limite){
+    document.forms["fInscription"].elements[suivant].focus();
+  }
+}
+
+function suivantEnter(event, suivant){
+	if(event.which == 13){
+		document.forms["fInscription"].elements[suivant].focus();
+	}
