e582b41694e3b9bd0dec316a59a5f46bab4f0cc6
remiboissise@gmail.com <remiboissise@gmail.com@df39fe93-acf2-2b65-9ade-a2c068ddeaf1>
2015-03-02 15:40:40 +0000

    bootstrap

--- trunk/include/menu_connexion.php
+++ trunk/include/menu_connexion.php
@@ -1,12 +1,13 @@
-<div id='menuConnexion'>
+	<div id='menuConnexion'>
 
-    <p>Connexion :</p>
+    	<p>Connexion :</p>
     
-    <input type="text" placeholder='Votre login' /></br>
-    <input type="text" placeholder='Votre mot de passe' /></br>
-    <input type="submit" value="OK" /></br>
-    
-    <a href="inscription.php">Pas encore inscrit?</a></br>
-    <a href="mot_de_passe_oublie.php">Mot de passe oubli&eacute;?</a></br>
+	    <input type="text" placeholder='Votre login' /></br>
+	    <input type="text" placeholder='Votre mot de passe' /></br>
+	    <input type="submit" value="OK" /></br>
+	    
+	    <a href="inscription.php">Pas encore inscrit?</a></br>
+	    <a href="mot_de_passe_oublie.php">Mot de passe oubli&eacute;?</a><br/>
 
-</div>
+	    
+	</div>
