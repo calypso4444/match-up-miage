e2f542e0c3868a170c2e6019eb84e3772bf3b7d6
remiboissise@gmail.com <remiboissise@gmail.com@df39fe93-acf2-2b65-9ade-a2c068ddeaf1>
2015-03-04 12:41:01 +0000

    modification page inscription

--- trunk/web/js/verificationInscription.js
+++ trunk/web/js/verificationInscription.js
@@ -22,76 +22,60 @@ $(document).ready(function(){
 	
 	form.submit(function(){
 		if(validatePseudo() & validateEmail() & validatePasse() & validatePasse2()){
-			alert("CONGRATULATION");
 			return true;
 		} else {
-			alert("FAILED");
 			return false;
 		}
 	});
 
 	function validatePseudo(){
 		if(!pseudo.val().match(/^[a-zA-Z0-9]{2,20}$/i)){
-			pseudo.addClass("error");
-			pseudoInfo.removeClass("good");
-			pseudoInfo.text("Votre pseudo doit contenir entre 2 et 20 caractères");
-			pseudoInfo.addClass("error");
+			pseudoInfo.removeClass("glyphicon glyphicon-ok");
+			pseudoInfo.addClass("glyphicon glyphicon-remove");
 			return false;
 		} else {
-			pseudo.removeClass("error");
-			pseudoInfo.addClass("good").text("Bon Pseudo");
-			pseudoInfo.removeClass("error");
+			pseudoInfo.removeClass("glyphicon glyphicon-remove");
+			pseudoInfo.addClass("glyphicon glyphicon-ok");
 			return true;
 		}
 	}
 	
 	function validateEmail(){
 			if(!email.val().match(/^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/i)){
-			email.addClass("error");
-			emailInfo.removeClass("good");
-			emailInfo.text("Mauvais email");
-			emailInfo.addClass("error");
+			emailInfo.removeClass("glyphicon glyphicon-ok");
+			emailInfo.addClass("glyphicon glyphicon-remove");
 			return false;
 		} else {
-			email.removeClass("error");
-			emailInfo.addClass("good").text("Bon email");
-			emailInfo.removeClass("error");
+			emailInfo.removeClass("glyphicon glyphicon-remove");
+			emailInfo.addClass("glyphicon glyphicon-ok");
 			return true;
 		}	
 	}
 	
 		function validatePasse(){
 			if(!passe.val().match(/^[a-zA-Z0-9]{8,}$/i)){
-			passe.addClass("error");
-			passeInfo.removeClass("good");
-			passeInfo.text("Votre mot de passe fait moins de 8 caractères");
-			passeInfo.addClass("error");
+			passeInfo.removeClass("glyphicon glyphicon-ok");
+			passeInfo.addClass("glyphicon glyphicon-remove");
 			return false;
 		} else {
-			passe.removeClass("error");
-			passeInfo.addClass("good").text("Bon mot de passe");
-			passeInfo.removeClass("error");
+			passeInfo.removeClass("glyphicon glyphicon-remove");
+			passeInfo.addClass("glyphicon glyphicon-ok");
 			return true;
 		}	
 	}
 	
 		function validatePasse2(){
 			if(!passe.val().match(/^[a-zA-Z0-9]{8,}$/i)){
-			passe2.addClass("error");
-			passe2Info.removeClass("good");
-			passe2Info.text("Le mot de passe ci-dessus ne respecte pas les conditions");
-			passe2Info.addClass("error");	
+			passe2Info.removeClass("glyphicon glyphicon-ok");
+			passe2Info.addClass("glyphicon glyphicon-remove");	
 			return false;
 		} else if(passe.val() !== passe2.val()){
-			passe2.addClass("error");
-			passe2Info.removeClass("good");
-			passe2Info.text("Ne correspond pas au mot de passe");
-			passe2Info.addClass("error");
+			passe2Info.removeClass("glyphicon glyphicon-ok");
+			passe2Info.addClass("glyphicon glyphicon-remove");
 			return false;
 		} else {
-			passe2.removeClass("error");
-			passe2Info.addClass("good").text("Bon");
-			passe2Info.removeClass("error");
+			passe2Info.removeClass("glyphicon glyphicon-remove");
+			passe2Info.addClass("glyphicon glyphicon-ok");
 			return true;
 		}	
 	}
