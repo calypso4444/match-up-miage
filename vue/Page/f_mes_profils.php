1591170c8205c0dc0aa070daa673d3cca8276e5e
lajungglespeedienne <lajungglespeedienne@gmail.com>
2015-03-05 19:16:03 +0000

    choix du profil a creer avec generation du lien de redirection vers une nouvelle page artiste ou salle

--- vue/Page/f_mes_profils.php
+++ vue/Page/f_mes_profils.php
@@ -3,6 +3,11 @@
 <div class="container">
     
     <h1>mes profils</h1>
-    <a href="profil.php" class="btn-block">Créer un nouveau profil en cliquant ici</a>
+    <form action="f_mes_profils.php" method="post">
+        <p>Créer un nouveau profil : </p>
+        <input type="submit" name="choixProfil" value="artiste">
+        <input type="submit" name="choixProfil" value="gérant">
+    </form>
+    <a href="<?php echo $vars['choix'];?>.php"><input type=button value='GO!'></a>
     
 </div>
