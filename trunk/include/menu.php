10fe5dc988aff4308632b602ca09f85f8e6551af
remiboissise@gmail.com <remiboissise@gmail.com@df39fe93-acf2-2b65-9ade-a2c068ddeaf1>
2015-03-05 10:41:21 +0000

    Aucun message

---
+++ trunk/include/menu.php
@@ -0,0 +1,23 @@
+<nav>
+
+    <ul class="menu">
+
+        <li><a href="index.php">Accueil</a></li>
+
+        <li><a href="que_faire.php">Que faire ?</a></li>
+
+        <li><a href="tous_les_artistes.php">Artistes</a></li>
+
+        <li><a href="toutes_les_salles.php">Salles</a></li>
+
+        <li><a href="petites_annonces.php">Petites annonces</a></li>	
+
+        <li>Recherche
+            <input type="text" placeholder='Tapez votre recherche...' />
+            <input type="submit" value="Valider" />
+            <input type="submit" value="+" />
+        </li>
+
+    </ul>
+
+</nav>
