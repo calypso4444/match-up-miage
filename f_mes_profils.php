e6fa3a88ffa639b6eac6445cc22abb9106a2ddfd
lajungglespeedienne <lajungglespeedienne@gmail.com>
2015-03-07 15:40:09 +0000

    referencement de tous les profils detenus par le user dans la section mes profils l'iamge du profil fait office d'hyperlien vers la page du profil correspondant

--- f_mes_profils.php
+++ f_mes_profils.php
@@ -8,13 +8,28 @@ include_once 'config/includeGlobal.php';
 /* fin de l'instanciation */
 
 /* séquence du controleur */
+
 $id=$_SESSION['user']['id'];
+
+//creation d'un nouveau profil
 $choix = filter_input(INPUT_POST, 'choixProfil');
 $noprofil=0;
 if (isset($choix)) {
     //on va interroger la base de données pour generer un numero identifier le profil qui va etre créé
+    if($choix==='artiste'){
     $noprofil=$model['GestionnaireProfil']->newProfilArtiste($id);
+    }
+    if($choix==='salle'){
+    $noprofil=$model['GestionnaireProfil']->newProfilSalle($id);
+    }
+    
 }
+
+//recuperation des profils salles
+$profilsSalle=$model['GestionnaireProfil']->getAllProfil_Salle($id);
+
+//recuperation des profils artistes
+$profilsArtiste=$model['GestionnaireProfil']->getAllProfil_Artiste($id);
 
 /* fin de séquence */
 
@@ -23,6 +38,8 @@ if (isset($choix)) {
 $vue = array();
 $vue['choix']=$choix;
 $vue['noprofil']=$noprofil;
+$vue['mesArtistes']=$profilsArtiste;
+$vue['mesSalles']=$profilsSalle;
 $view->render('f_mes_profils', $vue);
 
 /* fin de l'affichage de la vue */
--- modele/GestionnaireProfil.php
+++ modele/GestionnaireProfil.php
@@ -1,18 +1,51 @@
 <?php
 
 class GestionnaireProfil extends Gestionnaire {
-    public function newProfilArtiste($id){
+
+    public function newProfilArtiste($id) {
         mysqli_query($this->link, "INSERT INTO " . $GLOBALS['DB_TABLE']['ARTISTE'] . " (proprietaireArtiste) VALUES($id);");
-        $tmp=mysqli_query($this->link, "SELECT MAX(nArtiste)AS idmax FROM " . $GLOBALS['DB_TABLE']['ARTISTE'] . " WHERE proprietaireArtiste=$id");
+        $tmp = mysqli_query($this->link, "SELECT MAX(nArtiste)AS idmax FROM " . $GLOBALS['DB_TABLE']['ARTISTE'] . " WHERE proprietaireArtiste=$id");
         $row = mysqli_fetch_assoc($tmp);
         return $row['idmax'];
     }
-    
-    public function getAll($noProfil) {
+
+    public function newProfilSalle($id) {
+        mysqli_query($this->link, "INSERT INTO " . $GLOBALS['DB_TABLE']['SALLE'] . " (proprietaireSalle) VALUES($id);");
+        $tmp = mysqli_query($this->link, "SELECT MAX(nSalle)AS idmax FROM " . $GLOBALS['DB_TABLE']['SALLE'] . " WHERE proprietaireSalle=$id");
+        $row = mysqli_fetch_assoc($tmp);
+        return $row['idmax'];
+    }
+
+    public function getAllInfo_Artiste($noProfil) {
         $reqm = mysqli_query($this->link, "SELECT * FROM " . $GLOBALS['DB_TABLE']['ARTISTE'] . " WHERE nArtiste='$noProfil'");
         $row = mysqli_fetch_assoc($reqm);
-        return $row['nArtiste'] != null ? $row : null;
+        return $row != null ? $row : null;
     }
+
+    public function getAllInfo_Salle($noProfil) {
+        $reqm = mysqli_query($this->link, "SELECT * FROM " . $GLOBALS['DB_TABLE']['SALLE'] . " WHERE nSalle='$noProfil'");
+        $row = mysqli_fetch_assoc($reqm);
+        return $row != null ? $row : null;
+    }
+
+    public function getAllProfil_Artiste($id) {
+        $reqm = mysqli_query($this->link, "SELECT * FROM " . $GLOBALS['DB_TABLE']['ARTISTE'] . " WHERE proprietaireArtiste=$id");
+        $profils = array();
+        while ($row = mysqli_fetch_assoc($reqm)) {
+            $profils[] = $row;
+        }
+        return $profils;
+    }
+
+    public function getAllProfil_Salle($id) {
+        $reqm = mysqli_query($this->link, "SELECT * FROM " . $GLOBALS['DB_TABLE']['SALLE'] . " WHERE proprietaireSalle=$id");
+        $profils = array();
+        while ($row = mysqli_fetch_assoc($reqm)) {
+            $profils[] = $row;
+        }
+        return $profils;
+    }
+
 }
 
 ?>
