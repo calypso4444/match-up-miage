4919556007ef3d9a3dc33b66f48a39381aa79634
lajungglespeedienne@gmail.com <lajungglespeedienne@gmail.com@df39fe93-acf2-2b65-9ade-a2c068ddeaf1>
2015-03-07 12:59:23 +0000

    ajout des differentes bdd dans les configs globales

--- trunk/config/includeConfig.php
+++ trunk/config/includeConfig.php
@@ -11,6 +11,19 @@ $GLOBALS['DB_TABLE'] = array(
     'CONNEXION' => 'utilisateur',
     'VALIDATION' => 'validation',
     'ARTISTE' => 'artiste',
-    'SALLE' => 'salle'
+    'SALLE' => 'salle',
+    'CONCERT' => 'concert',
+    'ALBUM_PHOTO_A' => 'albumPhotoArtiste',
+    'ALBUM_PHOTO_S' => 'albumPhotoSalle',
+    'BIBLIOTHEQUE' => 'bibliothequeMusicale',
+    'ANNONCE_EVENEMENT_A' => 'annonceEvenementArtiste',
+    'ANNONCE_EVENEMENT_S' => 'annonceEvenementSalle',
+    'PETITE_ANNONCE' => 'petiteAnnonce',
+    'COMMENTAIRE_A' => 'commentaireArtiste',
+    'COMMENTAIRE_S' => 'commentaireSalle',
+    'EVENEMENT_SUIVI' => 'evenementSuivi',
+    'FAVORI_A' => 'favoriArtiste',
+    'FAVORI_S' => 'favoriSalle',
+    
 );
 ?>
