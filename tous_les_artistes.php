ba7df716e6cfc13b7777ccc2ec4d1f1d0b7ec505
lajungglespeedienne <lajungglespeedienne@gmail.com>
2015-03-08 10:57:54 +0000

    referencement de tous les artistes et de toutes les salles dans les pages dediées

--- tous_les_artistes.php
+++ tous_les_artistes.php
@@ -10,12 +10,15 @@ include_once 'config/includeGlobal.php';
 
 /* séquence du controleur */
 
+//recuperation des profils artistes
+$profilsArtiste=$model['GestionnaireProfil']->getAllProfil_Artiste();
 
 /* fin de séquence */
 
 /* affichage de la vue */
 
 $vue = array();
+$vue['profils_A']=$profilsArtiste;
 $view->render('tous_les_artistes', $vue);
 
 /* fin de l'affichage de la vue */
