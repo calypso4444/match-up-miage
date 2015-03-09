b2b7988d7f03e6366f410c8c94aba44aff24cf18
remiboissise@gmail.com <remiboissise@gmail.com@df39fe93-acf2-2b65-9ade-a2c068ddeaf1>
2015-03-09 21:38:09 +0000

    Modification de la page vue création artiste avec les champs important en premier et le reste (*)

--- trunk/vue/Page/modifier_salle.php
+++ trunk/vue/Page/modifier_salle.php
@@ -1,8 +1,11 @@
 <!-- vue/page -->
 <div class="container col-lg-12">
     <form id="formulaireModificationProfilSalle" method ="post" name="formulaireModificationProfilSalle" enctype="multipart/form-data">
+        
+        <!-- Début champs pour modifier son nom de salle -->
+        
         <div class="form-group">
-            <label class="control-label col-xs-4" for="nomSalle">Votre nom de salle :</label> 
+            <label class="control-label col-xs-4" for="nomSalle">Votre nom de salle : *</label> 
             <div class="col-xs-6">
                 <input class="form-control" id="nomArtiste" type="text" name="nomSalle" placeholder="<?php echo $vars['nomSalle']; ?>" value="<?php echo $vars['nomSalle']; ?>"/>
             </div>		
@@ -11,16 +14,11 @@
             </div>
             <div class="col-lg-2"></div>
         </div>
-        <div class="form-group">   
-            <label class="control-label col-xs-4" for="genreMusical">Votre genre musical :</label>
-            <div class="col-xs-6">
-                <input class="form-control" id="genreMusical" type="text" name="genreMusical" placeholder="<?php echo $vars['genreSalle']; ?>" value="<?php echo $vars['genreSalle']; ?>"/>
-            </div>
-            <div class="col-xs-1">
-                <span id="genreMusicalInfo"></span>
-            </div>
-            <div class="col-lg-2"></div>
-        </div>
+        
+        <!-- Fin champs pour modifier son nom de salle -->
+        
+        <!-- Début champs pour modifier son adresse -->
+        
         <div class="form-group">   
             <label class="control-label col-xs-4" for="adresseSalle">Adresse de la salle :</label>
             <div class="col-xs-6">
@@ -32,19 +30,9 @@
             <div class="col-lg-2"></div>
         </div>
         
+        <!-- Fin champs pour modifier son adresse -->
         
-        <div class="form-group">   
-            <label class="control-label col-xs-4" for="cpSalle">CP Salle :</label>
-            <div class="col-xs-6">
-                <input class="form-control" id="cpSalle" type="text" name="cpSalle" placeholder="<?php echo $vars['cpSalle']; ?>" value="<?php echo $vars['cpSalle']; ?>"/>
-            </div>
-            <div class="col-xs-1">
-                <span id="cpSalleInfo"></span>
-            </div>
-            <div class="col-lg-2"></div>
-        </div>
-        
-        
+        <!-- Début champs pour modifier la ville de la salle -->
         
 		<div class="form-group">   
             <label class="control-label col-xs-4" for="villeSalle">Ville :</label>
@@ -57,8 +45,25 @@
             <div class="col-lg-2"></div>
         </div>
         
+        <!-- Fin champs pour modifier la ville de la salle -->
         
+        <!-- Début champs pour modifier le code postal de la salle -->
         
+        <div class="form-group">   
+            <label class="control-label col-xs-4" for="cpSalle">CP Salle :</label>
+            <div class="col-xs-6">
+                <input class="form-control" id="cpSalle" type="text" name="cpSalle" placeholder="<?php echo $vars['cpSalle']; ?>" value="<?php echo $vars['cpSalle']; ?>"/>
+            </div>
+            <div class="col-xs-1">
+                <span id="cpSalleInfo"></span>
+            </div>
+            <div class="col-lg-2"></div>
+        </div>
+        
+        <!-- Fin champs pour modifier le code postal de la salle -->
+        
+        <!-- Début champs pour modifier le numéro de téléphone de la salle -->
+            
         <div class="form-group">   
             <label class="control-label col-xs-4" for="telSalle">Numéro de téléphone de la salle :</label>
             <div class="col-xs-6">
@@ -69,6 +74,11 @@
             </div>
             <div class="col-lg-2"></div>
         </div>
+        
+        <!-- Fin champs pour modifier le numéro de téléphone de la salle -->
+        
+        <!-- Début champs pour modifier le nom du gérant -->     
+        
         <div class="form-group">   
             <label class="control-label col-xs-4" for="nomGerant">Nom du gérant de la salle :</label>
             <div class="col-xs-6">
@@ -79,6 +89,11 @@
             </div>
             <div class="col-lg-2"></div>
         </div>
+        
+        <!-- Fin champs pour modifier le nom du gérant -->
+        
+        <!-- Début champs pour modifier le prénom du gérant -->      
+        
         <div class="form-group">   
             <label class="control-label col-xs-4" for="nomGerant">Prénom du gérant de la salle :</label>
             <div class="col-xs-6">
@@ -89,6 +104,11 @@
             </div>
             <div class="col-lg-2"></div>
         </div>
+        
+        <!-- Fin champs pour modifier le prénom du gérant -->
+        
+        <!-- Début champs pour modifier 'comment contacter le gérant de la salle' --> 
+ 
         <div class="form-group">   
             <label class="control-label col-xs-4" for="contactGerant">Comment contacter le gérant de la salle :</label>
             <div class="col-xs-6">
@@ -99,16 +119,40 @@
             </div>
             <div class="col-lg-2"></div>
         </div>
+        
+        <!-- Fin champs pour modifier 'comment contacter le gérant de la salle' -->
+        
+        <!-- Début champs pour modifier le genre musical --> 
+        
+        <div class="form-group">   
+            <label class="control-label col-xs-4" for="genreMusical">Votre genre musical :</label>
+            <div class="col-xs-6">
+                <input class="form-control" id="genreMusical" type="text" name="genreMusical" placeholder="<?php echo $vars['genreSalle']; ?>" value="<?php echo $vars['genreSalle']; ?>"/>
+            </div>
+            <div class="col-xs-1">
+                <span id="genreMusicalInfo"></span>
+            </div>
+            <div class="col-lg-2"></div>
+        </div>
+        
+        <!-- Fin champs pour modifier le genre musical -->
+        
+        <!-- Début champs pour modifier la description --> 
+     
         <div class="form-group">   
             <label class="control-label col-xs-4" for="descriptionSalle">Description :</label>
             <div class="col-xs-6">
-                <textarea class="form-control" id="descriptionSalle" type="text" name="descriptionSalle" placeholder="<?php echo $vars['descriptionSalle']; ?>" value="<?php echo $vars['descriptionSalle']; ?>"/></textarea>
+                <textarea class="form-control" id="descriptionSalle" rows="5" type="text" name="descriptionSalle" placeholder="<?php echo $vars['descriptionSalle']; ?>" value="<?php echo $vars['descriptionSalle']; ?>"/></textarea>
             </div>
             <div class="col-xs-1">
                 <span id="descriptionSalleInfo"></span>
             </div>
             <div class="col-lg-2"></div>
         </div>
+		
+		<!-- Fin champs pour modifier la description -->
+		
+		<!-- Début champs pour modifier la photo de la salle --> 
 
         <div class='form-group'>
             <label class="control-label col-xs-4" for="photoSalle">Votre photo de profil :</label> 
@@ -117,6 +161,9 @@
                 <input class="col-lg-offset-3" type="file" name="mon_fichier" id="mon_fichier" /><br/>
             </div>
         </div>
+        
+		<!-- Fin champs pour modifier la photo de la salle -->
+ 
         <div class='form-group'>
             <div class="col-xs-6">
             </div>
