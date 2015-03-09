4e2656ef2003dc81dd371ea3044be916d91b248d
remiboissise <remiboissise@gmail.com>
2015-03-09 22:27:12 +0000

    Commentaire + arrangement de position et textarea (JS marche parfaitement)

--- vue/Page/modifier_artiste.php
+++ vue/Page/modifier_artiste.php
@@ -1,6 +1,9 @@
 <!-- vue/page -->
 <div class="container col-lg-12">
-    <form id="formulaireModificationProfil" method ="post" name="formulaireModificationProfil" enctype="multipart/form-data">
+    <form id="formulaireModificationProfilArtiste" method ="post" name="formulaireModificationProfilArtiste" enctype="multipart/form-data">
+        
+        <!-- Début champs pour modifier le nom de l'artiste -->
+        
         <div class="form-group">
             <label class="control-label col-xs-4" for="nomArtiste">Votre nom de scene :</label> 
             <div class="col-xs-6">
@@ -11,16 +14,11 @@
             </div>
             <div class="col-lg-2"></div>
         </div>
-        <div class="form-group">   
-            <label class="control-label col-xs-4" for="descriptionArtiste">Description :</label>
-            <div class="col-xs-6">
-                <input class="form-control" id="descriptionArtiste" type="text" name="descriptionArtiste" placeholder="<?php echo $vars['descriptionArtiste']; ?>" value="<?php echo $vars['descriptionArtiste']; ?>"/>
-            </div>
-            <div class="col-xs-1">
-                <span id="descriptionArtisteInfo"></span>
-            </div>
-            <div class="col-lg-2"></div>
-        </div>
+        
+        <!-- Fin champs pour modifier le nom de l'artiste -->
+        
+        <!-- Début champs pour modifier le genre musical -->      
+        
         <div class="form-group">   
             <label class="control-label col-xs-4" for="genreMusical">Votre genre musical :</label>
             <div class="col-xs-6">
@@ -31,10 +29,33 @@
             </div>
             <div class="col-lg-2"></div>
         </div>
+        
+        <!-- Fin champs pour modifier le genre musical -->
+        
+        <!-- Début champs pour modifier la description de l'artiste -->      
+        
+        <div class="form-group">   
+            <label class="control-label col-xs-4" for="descriptionArtiste">Description :</label>
+            <div class="col-xs-6">
+                <textarea class="form-control" id="descriptionArtiste" type="text" rows="5" name="descriptionArtiste" placeholder="<?php echo $vars['descriptionArtiste']; ?>" value="<?php echo $vars['descriptionArtiste']; ?>"></textarea>
+            </div>
+            <div class="col-xs-1">
+                <span id="descriptionArtisteInfo"></span>
+            </div>
+            <div class="col-lg-2"></div>
+        </div>
+
+        <!-- Fin champs pour modifier la description de l'artiste -->
+           
+        <!-- Début champs pour modifier la photo Artiste -->
+        
         <label class="control-label col-xs-4" for="photoArtiste">Votre photo de profil :</label> 
         <input type="hidden" name="MAX_FILE_SIZE" value="1048576" />	
         <input class="col-lg-offset-3" type="file" name="mon_fichier" id="mon_fichier" /><br/>
         <div class="col-xs-1">
+	        
+	    <!-- Fin champs pour modifier la photo Artiste-->    
+	        
             <input class="btn btn-default" type="submit" value="Valider" id="envoyer"/>
         </div>
         <div class="col-xs-1">
