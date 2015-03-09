d41800f5aceb122b441ba25aed3ecbf720dee8fc
lajungglespeedienne <lajungglespeedienne@gmail.com>
2015-03-09 17:40:08 +0000

    ajout de boutos de retours vers la page mes profils

--- vue/Page/creation_artiste.php
+++ vue/Page/creation_artiste.php
@@ -2,9 +2,9 @@
 
 <div class="container col-lg-12">
     <form id="formulaireModificationProfilArtiste" method ="post" name="formulaireModificationProfilArtiste" enctype="multipart/form-data">
-        
-         <!-- Début champs pour entrer son nom d'artiste -->
-        
+
+        <!-- Début champs pour entrer son nom d'artiste -->
+
         <div class="form-group">
             <label class="control-label col-xs-4" for="nomArtiste">Votre nom de scene :</label> 
             <div class="col-xs-6">
@@ -15,11 +15,11 @@
             </div>
             <div class="col-lg-2"></div>
         </div>
-        
+
         <!-- Fin champs pour entrer son nom d'artiste -->
-        
+
         <!-- Début champs pour entrer son genre musical -->
-        
+
         <div class="form-group">   
             <label class="control-label col-xs-4" for="genreMusical">Votre genre musical :</label>
             <div class="col-xs-6">
@@ -30,11 +30,11 @@
             </div>
             <div class="col-lg-2"></div>
         </div>
-        
+
         <!-- Fin champs pour entrer son genre musical -->          
-                  
+
         <!-- Début champs pour entrer sa description -->  
-                     
+
         <div class="form-group">   
             <label class="control-label col-xs-4" for="descriptionArtiste">Description :</label>
             <div class="col-xs-6">
@@ -45,19 +45,24 @@
             </div>
             <div class="col-lg-2"></div>
         </div>
-        
+
         <!-- Fin champs pour entrer sa description -->
-        
+
         <!-- Début champs pour choisir sa photo de profil --> 
-               
+
         <label class="control-label col-xs-4" for="photoArtiste">Votre photo de profil :</label> 
         <input type="hidden" name="MAX_FILE_SIZE" value="1048576" />	
         <input class="col-lg-offset-3" type="file" name="mon_fichier" id="mon_fichier" /><br/>
         <div>
-	        
-	    <!-- Fin champs pour choisir sa photo de profil --> 
-	        
+
+            <!-- Fin champs pour choisir sa photo de profil --> 
+
             <input class="btn btn-default" type="submit" value="Valider" id="envoyer"/>
+
+            <div class="col-xs-1">
+                <a class="btn btn-default" href="f_mes_profils.php">Retour à mes profils</a>
+            </div>
+
         </div>
     </form>  
 </div>
