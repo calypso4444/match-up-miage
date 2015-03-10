d32274750af30867dd7d685fbdfddbac736daa3d
lajungglespeedienne <lajungglespeedienne@gmail.com>
2015-03-10 13:05:57 +0000

    insertion de commentaire ok

--- artiste.php
+++ artiste.php
@@ -1,36 +1,38 @@
 <!-- web -->
 
 <?php
-
 /* instanciation des fichiers de config + modele */
 
 include_once 'config/includeGlobal.php';
 
 /* fin de l'instanciation */
 
-/* séquence du controleur */ 
-$noProfil=filter_input(INPUT_GET, 'tmp');
-$infoProfil=$model['GestionnaireProfil']->getAllInfo_Artiste($noProfil);
-$nomProfil=$infoProfil['nomArtiste'];
-$photoProfil=$infoProfil['photoProfilArtiste'];
-$descProfil=$infoProfil['descriptionArtiste'];
+/* séquence du controleur */
+$noProfil = filter_input(INPUT_GET, 'tmp');
+$infoProfil = $model['GestionnaireProfil']->getAllInfo_Artiste($noProfil);
+$nomProfil = $infoProfil['nomArtiste'];
+$photoProfil = $infoProfil['photoProfilArtiste'];
+$descProfil = $infoProfil['descriptionArtiste'];
 
-$id=$_SESSION['user']['id'];
+$id = $_SESSION['user']['id'];
 
-$favori=filter_input(INPUT_POST, 'favori');
-    if($favori==="true"){
-        $model['GestionnaireUtilisateur']->ajouterEnFavoriArtiste($noProfil, $id);
-    }
+$favori = filter_input(INPUT_POST, 'favori');
+if ($favori === "true") {
+    $model['GestionnaireUtilisateur']->ajouterEnFavoriArtiste($noProfil, $id);
+}
+
+$commentaires = $model['GestionnaireCommentaire']->getAllCommentairesByIdArtiste($noProfil);
 
 /* fin de séquence */
 
 /* affichage de la vue */
 
 $vue = array();
-$vue['noProfil']=$noProfil;
-$vue['nomProfil']=$nomProfil;
-$vue['photoProfil']=$photoProfil;
-$vue['descProfil']=$descProfil;
+$vue['noProfil'] = $noProfil;
+$vue['nomProfil'] = $nomProfil;
+$vue['photoProfil'] = $photoProfil;
+$vue['descProfil'] = $descProfil;
+$vue['commentaire']=$commentaires;
 $view->render('artiste', $vue);
 
 /* fin de l'affichage de la vue */
--- modele/GestionnaireCommentaire.php
+++ modele/GestionnaireCommentaire.php
@@ -1,11 +1,11 @@
 <?php
 
 class GestionnaireCommentaire extends Gestionnaire {
-    public function getAllCommentairesByIdSalle($noProfil){
-        $reqm = mysqli_query($this->link, 
-                "SELECT nCommentaireSalle,cible,texteCommentaireSalle,dateEditionCommentaireSalle, auteur, pseudo, avatar "
+
+    public function getAllCommentairesByIdSalle($noProfil) {
+        $reqm = mysqli_query($this->link, "SELECT nCommentaireSalle,cible,texteCommentaireSalle,dateEditionCommentaireSalle, auteur, pseudo, avatar "
                 . " FROM " . $GLOBALS['DB_TABLE']['COMMENTAIRE_S'] . " "
-                . " INNER JOIN ". $GLOBALS['DB_TABLE']['CONNEXION'] . " "
+                . " INNER JOIN " . $GLOBALS['DB_TABLE']['CONNEXION'] . " "
                 . " ON auteur=id "
                 . "WHERE cible=$noProfil");
         $commentaires = array();
@@ -14,10 +14,32 @@ class GestionnaireCommentaire extends Gestionnaire {
         }
         return $commentaires;
     }
-    
-    public function getLastCommentaire(){
+
+    public function getAllCommentairesByIdArtiste($noProfil) {
+        $reqm = mysqli_query($this->link, "SELECT nCommentaireArtiste,cible,texteCommentaireArtiste,dateEditionCommentaireArtiste, auteur, pseudo, avatar "
+                . " FROM " . $GLOBALS['DB_TABLE']['COMMENTAIRE_A'] . " "
+                . " INNER JOIN " . $GLOBALS['DB_TABLE']['CONNEXION'] . " "
+                . " ON auteur=id "
+                . "WHERE cible=$noProfil");
+        $commentaires = array();
+        while ($row = mysqli_fetch_assoc($reqm)) {
+            $commentaires[] = $row;
+        }
+        return $commentaires;
+    }
+
+    public function getLastCommentaire() {
         
     }
+
+    public function commenterSalle($noProfil, $id, $texte) {
+        mysqli_query($this->link, "INSERT INTO " . $GLOBALS['DB_TABLE']['COMMENTAIRE_S'] . "  (nCommentaireSalle, cible, texteCommentaireSalle, auteur, dateEditionCommentaireSalle) VALUES('',$noProfil,'$texte',$id ,NOW());");
+    }
+    
+    public function commenterArtiste($noProfil, $id, $texte) {
+        mysqli_query($this->link, "INSERT INTO " . $GLOBALS['DB_TABLE']['COMMENTAIRE_A'] . "  (nCommentaireArtiste, cible, texteCommentaireArtiste, auteur, dateEditionCommentaireArtiste) VALUES('',$noProfil,'$texte',$id ,NOW());");
+    }
+
 }
 
 ?>
