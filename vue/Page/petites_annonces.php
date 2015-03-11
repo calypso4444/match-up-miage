78aba0898fe4960165cae4c742fcd5ccb03f0757
lajungglespeedienne <lajungglespeedienne@gmail.com>
2015-03-11 15:53:11 +0000

    affichage et referencement des petites annonces par date dans la page petites annonces

--- vue/Page/petites_annonces.php
+++ vue/Page/petites_annonces.php
@@ -4,4 +4,31 @@
     
     <h1>petites annonces</h1>
     
+    <div id="lesPetitesAnnonces">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Posté par</th>
+                    <th>Annonce</th>
+                    <th>Date</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($vars['annonces'] as $annonces):
+                    echo '<tr>';
+                    echo "<td class='col-lg-3'><a href=salle.php?tmp=" . $annonces['auteur'] . ">".$annonces['nomSalle']."</a></td>";
+                    echo '<td>entre le ' . $annonces['dateDeb'] . 'et le '.$annonces['dateFin'].'</td>';
+                    echo '<td>' . $annonces['dateEditionPetiteAnnonce'] .'</td>';
+                    echo '<td>'
+                    . '<a class="btn" href="petites_annonces.php?nSalle=' . $annonces['auteur'] . '&nAnnonce=' . $annonces['nPetiteAnnonce'] . '">Postuler</a>'
+                    . '</td>';
+                    echo '</tr>';
+                endforeach;
+                ?>
+            </tbody>
+        </table>
+    </div>
+    
 </div>
