4ec036c07d85dbf9b0de8c2c479da897f634a564
remiboissise@gmail.com <remiboissise@gmail.com@df39fe93-acf2-2b65-9ade-a2c068ddeaf1>
2015-03-11 15:42:36 +0000

    Voilà la carte

--- trunk/web/js/map.js
+++ trunk/web/js/map.js
@@ -1,16 +1,52 @@
-
 $(document).ready(function() {	
-	var adresse = $("#adresse").val();
 	
-	$('#map-canvas').gmap3({	
-		 marker:{
-      address: adresse
-          },
-    map:{
-      options:{
-        zoom: 16
-      }
-    }
+	var adresseE = $("#adresseE").val();
+	var nomSalle = $("#nomSalle").val();
+	var adresse = $("#adresse").val();
+	var ville = $("#ville").val();
+	var codepostal = $("#codepostal").val();
+	
+	
+	$("#map-canvas").gmap3({
+	
+		marker:{
+		values:[
+			{address:adresseE, data:nomSalle + "<br/>" + adresse + "<br/>" + codepostal + "<br/>" + ville},
+		],
 		
+		options:{
+		draggable: false
+		},
+		
+		events:{
+			mouseover: function(marker, event, context){
+			var map = $(this).gmap3("get"),
+			infowindow = $(this).gmap3({get:{name:"infowindow"}});
+			if (infowindow){
+				infowindow.open(map, marker);
+				infowindow.setContent(context.data);
+			} else {
+				$(this).gmap3({
+					infowindow:{
+						anchor:marker, 
+						options:{content: context.data}
+					}
+          		});
+        	}
+      		},
+      		
+	  		mouseout: function(){
+	  		var infowindow = $(this).gmap3({get:{name:"infowindow"}});
+	  		if (infowindow){
+	  			infowindow.close();
+        	}
+      		}
+    	}
+		},
+			map:{
+				options:{
+					zoom: 15
+    			}
+  			}
 	});	
 });
