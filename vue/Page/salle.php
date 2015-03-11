7caa5c74279aa3463f92c396c27347670a8310f0
remiboissise <remiboissise@gmail.com>
2015-03-11 15:42:36 +0000

    Voilà la carte

--- vue/Page/salle.php
+++ vue/Page/salle.php
@@ -1,7 +1,15 @@
 <!-- vue/page -->
 
 <div class="col-lg-12">
-
+	
+	<!-- Permet de récupérer nos informations pour notre carte -->
+	<input id="adresseE" type="hidden" value="<?php echo $vars['adresse'] . " " . $vars['cp'] . " " . $vars['ville']; ?>"></input>
+	<input id="nomSalle" type="hidden" value="<?php echo $vars['nomProfil']; ?>"></input>
+	<input id="adresse" type="hidden" value="<?php echo $vars['adresse']; ?>"></input>
+	<input id="ville" type="hidden" value="<?php echo $vars['ville']; ?>"></input>
+	<input id="codepostal" type="hidden" value="<?php echo $vars['cp']; ?>"></input>
+	<!-- FIN -->
+	
     <h1><?php echo $vars['nomProfil']; ?></h1>
 
     <div id="photoProfil" class="col-lg-12">
@@ -55,8 +63,7 @@
             <div id="acces">
                 <h4>L'accès : </h4>
                 <?php echo $vars['adresse'] . " " . $vars['cp'] . " " . $vars['ville']; ?>
-                <input id="adresse" type="hidden" value="<?php echo $vars['adresse'] . " " . $vars['cp'] . " " . $vars['ville']; ?>"></input>
-                <div id="map-canvas"></div>
+                                <div id="map-canvas"></div>
             </div>
         </aside>
         <section class="col-lg-9">
