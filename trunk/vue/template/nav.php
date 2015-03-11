4b88c1c529402a8bd3c37100dbfc9be07c473104
lajungglespeedienne@gmail.com <lajungglespeedienne@gmail.com@df39fe93-acf2-2b65-9ade-a2c068ddeaf1>
2015-03-11 17:42:48 +0000

    fixation du navbar

--- trunk/vue/template/nav.php
+++ trunk/vue/template/nav.php
@@ -1,3 +1,21 @@
+<nav>
+                </br>
+                <ul class="menu navbar nav-justified navbar-form ">
+                    <li class="text-center"><a href="index.php">Accueil</a></li>
+                    <li class="text-center"><a href="que_faire.php">Que faire ?</a></li>
+                    <li class="text-center"><a href="tous_les_artistes.php">Artistes</a></li>
+                    <li class="text-center"><a href="toutes_les_salles.php">Salles</a></li>
+                    <li class="text-center"><a href="petites_annonces.php">Petites annonces</a></li>	
+                    <li class="text-center">
+                        <form id="formulaireRecherche" method ="post" name="recherche">
+                            <label for="recherche"></label> 
+                            <input id="recherche" type="text" name="recherche" placeholder="Tapez votre recherche ici" value=""/>
+                            <input  type="submit" value="GO" id="envoyer"/>
+                            <a href="f_recherche_avancee.php" class="glyphicon glyphicon-plus"></a>
+                        </form>
+                    </li>
+                </ul>
+
+            </nav>
 
 
-<div id="main" class="container-fluid col-lg-12">
