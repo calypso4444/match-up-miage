b2dfe36a942dfa9c5b32651c64da662c8232e67a
lajungglespeedienne@gmail.com <lajungglespeedienne@gmail.com@df39fe93-acf2-2b65-9ade-a2c068ddeaf1>
2015-03-12 11:42:52 +0000

    ajout de photo dans les albums photos salle.php et artiste.php depuis les pages

--- trunk/artiste.php
+++ trunk/artiste.php
@@ -40,6 +40,32 @@ $commentaires = $model['GestionnaireCommentaire']->getAllCommentairesByIdArtiste
 
 $albumPhoto = $model['GestionnaireProfil']->getAllPhotoArtisteById($noProfil);
 
+if (isset($_FILES['mon_fichier'])) {
+    $tab_img = $_FILES['mon_fichier'];
+    if ($_FILES['mon_fichier']['error'] > 0) {
+        $erreur = "Erreur lors du transfert";
+    }
+    $extensions_valides = array('jpg', 'jpeg', 'gif', 'png');
+    //1. strrchr renvoie l'extension avec le point (« . »).
+    //2. substr(chaine,1) ignore le premier caractère de chaine.
+    //3. strtolower met l'extension en minuscules.
+    $extension_upload = strtolower(substr(strrchr($tab_img['name'], '.'), 1));
+//    $maxwidth = 0;
+//    $maxheight = 0;
+//    $image_sizes = getimagesize($tab_img['tmp_name']);
+//    if ($image_sizes[0] > $maxwidth OR $image_sizes[1] > $maxheight) {
+//        $erreur = "Image trop grande";
+//    }
+    $idmax = $model['GestionnaireProfil']->getNMaxPhotoArtiste($noProfil);
+    $idmax++;
+    $chemin = "web/image/albumPhotoArtiste/{$noProfil}.{$idmax}.{$extension_upload}";
+    $resultat = move_uploaded_file($tab_img['tmp_name'], $chemin);
+    if ($resultat) {
+        $model['GestionnaireProfil']->ajouterPhotoArtiste($noProfil, $chemin);
+        $albumPhoto = $model['GestionnaireProfil']->getAllPhotoArtisteById($noProfil);
+    }
+}
+
 /* fin de séquence */
 
 /* affichage de la vue */
