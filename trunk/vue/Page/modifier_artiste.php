26f61067d84bd6e11e729d60b5a42ba0b8b6d4b3
remiboissise@gmail.com <remiboissise@gmail.com@df39fe93-acf2-2b65-9ade-a2c068ddeaf1>
2015-03-14 17:11:36 +0000

    Modification de la page de modfication artiste

--- trunk/vue/Page/modifier_artiste.php
+++ trunk/vue/Page/modifier_artiste.php
@@ -1,65 +1,87 @@
 <!-- vue/page -->
-<div class="container col-lg-12">
-    <form id="formulaireModificationProfilArtiste" method ="post" name="formulaireModificationProfilArtiste" enctype="multipart/form-data">
-        
-        <!-- Début champs pour modifier le nom de l'artiste -->
-        
-        <div class="form-group">
-            <label class="control-label col-xs-4" for="nomArtiste">Votre nom de scene :</label> 
-            <div class="col-xs-6">
-                <input class="form-control" id="nomArtiste" type="text" name="nomArtiste" placeholder="<?php echo $vars['nomArtiste']; ?>" value="<?php echo $vars['nomArtiste']; ?>"/>
-            </div>		
-            <div class="col-xs-1">	
-                <span id="nomArtisteInfo"></span>
-            </div>
-            <div class="col-lg-2"></div>
-        </div>
-        
-        <!-- Fin champs pour modifier le nom de l'artiste -->
-        
-        <!-- Début champs pour modifier le genre musical -->      
-        
-        <div class="form-group">   
-            <label class="control-label col-xs-4" for="genreMusical">Votre genre musical :</label>
-            <div class="col-xs-6">
-                <input class="form-control" id="genreMusical" type="text" name="genreMusical" placeholder="<?php echo $vars['genreArtiste']; ?>" value="<?php echo $vars['genreArtiste']; ?>"/>
-            </div>
-            <div class="col-xs-1">
-                <span id="genreMusicalInfo"></span>
-            </div>
-            <div class="col-lg-2"></div>
-        </div>
-        
-        <!-- Fin champs pour modifier le genre musical -->
-        
-        <!-- Début champs pour modifier la description de l'artiste -->      
-        
-        <div class="form-group">   
-            <label class="control-label col-xs-4" for="descriptionArtiste">Description :</label>
-            <div class="col-xs-6">
-                <textarea class="form-control" id="descriptionArtiste" type="text" rows="5" name="descriptionArtiste" placeholder="<?php echo $vars['descriptionArtiste']; ?>" value="<?php echo $vars['descriptionArtiste']; ?>"></textarea>
-            </div>
-            <div class="col-xs-1">
-                <span id="descriptionArtisteInfo"></span>
-            </div>
-            <div class="col-lg-2"></div>
-        </div>
 
-        <!-- Fin champs pour modifier la description de l'artiste -->
-           
-        <!-- Début champs pour modifier la photo Artiste -->
-        
-        <label class="control-label col-xs-4" for="photoArtiste">Votre photo de profil :</label> 
-        <input type="hidden" name="MAX_FILE_SIZE" value="1048576" />	
-        <input class="col-lg-offset-3" type="file" name="mon_fichier" id="mon_fichier" /><br/>
-        <div class="col-xs-1">
-	        
-	    <!-- Fin champs pour modifier la photo Artiste-->    
-	        
-            <input class="btn btn-default" type="submit" value="Valider" id="envoyer"/>
-        </div>
-        <div class="col-xs-1">
-            <a class="btn btn-default" href="f_mes_profils.php">Retour à mes profils</a>
+<div class="container col-lg-12">
+	<h1>Création de la page artiste</h1>
+    <form id="formulaireModificationProfilArtiste" method ="post" name="formulaireModificationProfilArtiste" enctype="multipart/form-data">
+	    <div id="row">    
+		   <div class="col-lg-offset-2 col-lg-8 col-lg-offset-2">
+		   		<table class="table">
+		   		 	<tbody>
+			    		        
+
+        <!-- Début champs pour entrer son nom d'artiste -->
+
+        <tr>
+	        <td>
+            	<label class="control-label" for="nomArtiste">Votre nom de scène :</label> 
+	        </td>
+	        <td>
+                <input class="form-control" id="nomArtiste" type="text" name="nomArtiste" placeholder="<?php echo $vars['nomArtiste']; ?>" value="<?php echo $vars['nomArtiste']; ?>"/>
+	        </td>
+	        <td>	
+                <span id="nomArtisteInfo"></span>
+            </td>
+        </tr>
+
+        <!-- Fin champs pour entrer son nom d'artiste -->
+
+        <!-- Début champs pour entrer son genre musical -->
+
+        <tr>
+	        <td> 
+            	<label class="control-label" for="genreMusical">Votre genre musical :</label>
+	        </td>
+	        <td>
+                <input class="form-control" id="genreMusical" type="text" name="genreMusical" placeholder="<?php echo $vars['genreArtiste']; ?>" value="<?php echo $vars['genreArtiste']; ?>"/>
+	        </td>
+	        <td>
+                <span id="genreMusicalInfo"></span>
+	        </td>
+        </tr>
+
+        <!-- Fin champs pour entrer son genre musical -->          
+
+        <!-- Début champs pour entrer sa description -->  
+
+		<tr>
+			<td>
+            	<label class="control-label" for="descriptionArtiste">Description :</label><br/>
+            	<span id="descriptionArtisteInfo"></span>
+			</td>
+			<td>
+                <textarea style="resize:none" class="form-control" rows="5" id="descriptionArtiste" type="text" name="descriptionArtiste" placeholder="<?php echo $vars['descriptionArtiste']; ?>" value="<?php echo $vars['descriptionArtiste']; ?>"/></textarea>
+            </td>
+        </tr>
+
+        <!-- Fin champs pour entrer sa description -->
+
+        <!-- Début champs pour choisir sa photo de profil --> 
+
+
+		<tr>
+			<td>
+        		<label class="control-label" for="photoArtiste">Votre photo de profil :</label> 
+			</td>
+			<td>
+				<input type="hidden" name="MAX_FILE_SIZE" value="1048576" />	
+				<input class="filestyle" type="file" name="mon_fichier" id="mon_fichier" data-input="false" data-buttonText="Votre photo"/><br/>
+			</td>
+		</tr>
+
+            <!-- Fin champs pour choisir sa photo de profil --> 
+
+		<tr>
+	        <td>
+		        <a class="btn btn-default" href="f_mes_profils.php">Retour à mes profils</a>  
+            </td>
+            <td>
+				<input id="envoyer" class="btn btn-default" type="submit" value="Valider"/>
+            </td>
+        </tr>
+       
+        			
+        		</table>
+        	</div>
         </div>
     </form>  
 </div>
