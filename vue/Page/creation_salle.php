767182675b546deabc42a9a44034fe06d0024962
remiboissise <remiboissise@gmail.com>
2015-03-14 16:23:06 +0000

    Modification de la page de création de la salle

--- vue/Page/creation_salle.php
+++ vue/Page/creation_salle.php
@@ -1,180 +1,209 @@
 <!-- vue/page -->
-<div class="container col-lg-12">
-    <form id="formulaireModificationProfilSalle" method ="post" name="formulaireModificationProfilSalle" enctype="multipart/form-data">
-	    
-	     <!-- Début champs pour entrer son nom de salle -->
-	     
-        <div class="form-group">
-            <label class="control-label col-xs-4" for="nomSalle">Votre nom de salle : *</label> 
-            <div class="col-xs-6">
-                <input class="form-control" id="nomSalle" type="text" name="nomSalle" placeholder="" value=""/>
-            </div>		
-            <div class="col-xs-1">	
-                <span id="nomSalleInfo"></span>
-            </div>
-            <div class="col-lg-2"></div>
-        </div>
-        
-        <!-- Fin champs pour entrer son nom de salle -->
-                
-        <!-- Début champs pour entrer l'adresse de la salle -->
-                
-        <div class="form-group">   
-            <label class="control-label col-xs-4" for="adresseSalle">Adresse de la salle : *</label>
-            <div class="col-xs-6">
-                <input class="form-control" id="adresseSalle" type="text" name="adresseSalle" placeholder="" value=""/>
-            </div>
-            <div class="col-xs-1">
-                <span id="adresseSalleInfo"></span>
-            </div>
-            <div class="col-lg-2"></div>
-        </div>
-        
-        <!-- Fin champs pour entrer l'adresse de la salle -->   
-
-        <!-- Début champs pour modifier votre ville -->
-             
-        <div class="form-group">   
-            <label class="control-label col-xs-4" for="villeSalle">Votre ville : *</label>
-            <div class="col-xs-6">
-                <input class="form-control" id="villeSalle" type="text" name="villeSalle" placeholder="" value=""/>
-            </div>
-            <div class="col-xs-1">
-                <span id="villeSalleInfo"></span>
-            </div>
-            <div class="col-lg-2"></div>
-		</div>
+<div class="col-lg-12">	
+	
+	<h1> Création de votre salle </h1>
 		
-        <!-- Fin champs pour modifier votre ville --> 
-                               
-        <!-- Début champs pour modifier son code postal -->
+    <form id="formulaireModificationProfilSalle" method ="post" name="formulaireModificationProfilSalle" enctype="multipart/form-data">
+	    <div id="row">    
+		   <div class="col-lg-6">
+		   		<table class="table">
+		   		 	<tbody>
+			    		        
+        <!-- Début champs pour modifier son nom de salle -->
         
-        <div class="form-group">   
-            <label class="control-label col-xs-4" for="cpSalle">Votre code postal : *</label>
-            <div class="col-xs-6">
+        <tr>
+	        <td>
+            <label class="control-label" for="nomSalle">Votre nom de salle : *</label> 
+	        </td>
+	        <td>
+                <input class="form-control" id="nomSalle" type="text" name="nomSalle" placeholder="" value=""/>
+	        </td>
+	        <td>		
+                <span id="nomSalleInfo"></span>
+	        </td>
+        </tr>
+        
+        <!-- Fin champs pour modifier son nom de salle -->
+        
+        <!-- Début champs pour modifier son adresse -->
+        
+        <tr> 
+	        <td>  
+            <label class="control-label" for="adresseSalle">Adresse de la salle : *</label>
+	        </td>
+	        <td>
+                <input class="form-control" id="adresseSalle" type="text" name="adresseSalle" placeholder="" value=""/>
+            </td>
+            <td>
+                <span id="adresseSalleInfo"></span>
+            </td>
+        </tr>
+        
+        <!-- Fin champs pour modifier son adresse -->
+        
+        <!-- Début champs pour modifier la ville de la salle -->
+        
+		<tr> 
+			<td>
+            <label class="control-label" for="villeSalle">Ville : *</label>
+			</td>
+			<td>
+                <input class="form-control" id="villeSalle" type="text" name="villeSalle" placeholder="" value=""/>
+            </td>
+			<td>
+                <span id="villeSalleInfo"></span>
+            </td>
+        </tr>
+        
+        <!-- Fin champs pour modifier la ville de la salle -->
+        
+        <!-- Début champs pour modifier le code postal de la salle -->
+        
+        <tr>  
+	        <td> 
+            <label class="control-label" for="cpSalle">CP Salle : *</label>
+	        </td>
+	        <td>
                 <input class="form-control" id="cpSalle" type="text" name="cpSalle" placeholder="" value=""/>
-            </div>
-            <div class="col-xs-1">
+            </td>
+            <td>
                 <span id="cpSalleInfo"></span>
-            </div>
-            <div class="col-lg-2"></div>
-        </div>
-     
-        <!-- Fin champs pour modifier son code postal -->
+            </td>
+        </tr>
         
-        <!-- Début champs pour entrer le numéro de téléphone de la salle -->
+        <!-- Début champs pour modifier le genre musical --> 
         
-        <div class="form-group">   
-            <label class="control-label col-xs-4" for="telSalle">Numéro de téléphone de la salle :</label>
-            <div class="col-xs-6">
-                <input class="form-control" id="telSalle" type="text" name="telSalle" placeholder="" value=""/>
-            </div>
-            <div class="col-xs-1">
-                <span id="telSalleInfo"></span>
-            </div>
-            <div class="col-lg-2"></div>
-        </div>        
-        
-        <!-- Fin champs pour entrer le numéro de téléphone de la salle -->
-        
-        <!-- Début champs pour entrer le nom du gérant -->
-        
-        <div class="form-group">   
-            <label class="control-label col-xs-4" for="nomGerant">Nom du gérant de la salle :</label>
-            <div class="col-xs-6">
-                <input class="form-control" id="nomGerant" type="text" name="nomGerant" placeholder="" value=""/>
-            </div>
-            <div class="col-xs-1">
-                <span id="nomGerantInfo"></span>
-            </div>
-            <div class="col-lg-2"></div>
-        </div>        
-        
-        <!-- Fin champs pour entrer le nom du gérant -->
-        
-        <!-- Début champs pour entrer le prénom du gérant -->
-                
-        <div class="form-group">   
-            <label class="control-label col-xs-4" for="prenomGerant">Prénom du gérant de la salle :</label>
-            <div class="col-xs-6">
-                <input class="form-control" id="prenomGerant" type="text" name="prenomGerant" placeholder="" value=""/>
-            </div>
-            <div class="col-xs-1">
-                <span id="prenomGerantInfo"></span>
-            </div>
-            <div class="col-lg-2"></div>
-        </div>
-        
-        <!-- Fin champs pour entrer le prénom du gérant -->
-        
-        <!-- Début champs pour entrer le contact gérant -->
-              
-        <div class="form-group">   
-            <label class="control-label col-xs-4" for="contactGerant">Comment contacter le gérant de la salle :</label>
-            <div class="col-xs-6">
-                <input class="form-control" id="contactGerant" type="text" name="contactGerant" placeholder="" value=""/>
-            </div>
-            <div class="col-xs-1">
-                <span id="contactGerantInfo"></span>
-            </div>
-            <div class="col-lg-2"></div>
-        </div>       
-        
-        <!-- Fin champs pour entrer le contact gérant -->      
-                
-        <!-- Début champs pour entrer son genre musical -->
-        
-        <div class="form-group">   
-            <label class="control-label col-xs-4" for="genreMusical">Votre genre musical :</label>
-            <div class="col-xs-6">
+        <tr> 
+	        <td>
+            <label class="control-label" for="genreMusical">Votre genre musical :</label>
+	        </td>
+	        <td>
                 <input class="form-control" id="genreMusical" type="text" name="genreMusical" placeholder="" value=""/>
-            </div>
-            <div class="col-xs-1">
+	        </td>
+	        <td>
                 <span id="genreMusicalInfo"></span>
-            </div>
-            <div class="col-lg-2"></div>
-        </div>
+            </td>
+        </tr>
         
-        <!-- Fin champs pour entrer son genre musical -->
-                   
-        <!-- Début champs pour entrer la description -->
-        
-        <div class="form-group">   
-            <label class="control-label col-xs-4" for="descriptionSalle">Description :</label>
-            <div class="col-xs-6">
-                <textarea class="form-control" rows="5" id="descriptionSalle" type="text" name="descriptionSalle" placeholder="" value=""/></textarea>
-            </div>
-            <div class="col-xs-1">
-                <span id="descriptionSalleInfo"></span>
-            </div>
-            <div class="col-lg-2"></div>
-        </div>
-        
-        <!-- Fin champs pour entrer la description -->
-        
-        <!-- Début champs pour ajouter une photo de profil -->        
-        
-        <div class='form-group'>
-            <label class="control-label col-xs-4" for="photoSalle">Votre photo de profil :</label> 
-            <div class="col-xs-6">
+        <!-- Fin champs pour modifier le genre musical -->
+       
+        <!-- Début champs pour modifier la description --> 
+     
+        <tr> 
+	        <td>
+            <label class="control-label" for="descriptionSalle">Description :</label><br/>
+	        <span id="descriptionSalleInfo"></span>
+	        </td>
+	        <td>
+                <textarea style="resize:none" class="form-control" id="descriptionSalle" rows="5" type="text" name="descriptionSalle" placeholder="" value=""></textarea>
+            </td>
+
+        </tr>
+		
+		<!-- Fin champs pour modifier la description -->
+		
+		<!-- Début champs pour modifier la photo de la salle --> 
+
+        <tr>
+	        <td>
+            <label class="control-label" for="photoSalle">Votre photo de profil :</label> 
+	        </td>
+	        <td>
                 <input type="hidden" name="MAX_FILE_SIZE" value="1048576" />	
-                <input class="col-lg-offset-3" type="file" name="mon_fichier" id="mon_fichier" /><br/>
-            </div>
-        </div>
+                <input class="filestyle" type="file" name="mon_fichier" id="mon_fichier" data-input="false" data-buttonText="Votre photo"/>
+            </td>
+        </tr>
+
+		<!-- Fin champs pour modifier la photo de la salle -->
         
-        <!-- Fin champs pour ajouter une photo de profil -->
-               
-        <div class='form-group'>
-            <div class="col-xs-6">
-            </div>
-            <div class="col-xs-1">
-                <input class="btn btn-default" type="submit" value="Valider" id="envoyer"/>
-            </div>
-            <div class="col-xs-1">
-                <a class="btn btn-default" href="f_mes_profils.php">Retour à mes profils</a>
-            </div>
-            <div class="col-lg-2"></div>
-        </div>
+		   		 	</tbody>
+		   		</table>
+		   </div>
+         <div class="col-lg-6">
+		   	<table class="table">
+			   	<tbody>
+        
+        <!-- Fin champs pour modifier le code postal de la salle -->
+        
+        <!-- Début champs pour modifier le numéro de téléphone de la salle -->
+            
+        <tr>
+	        <td>
+            <label class="control-label" for="telSalle">Numéro de téléphone de la salle :</label>
+            </td>
+            <td>
+                <input class="form-control" id="telSalle" type="text" name="telSalle" placeholder="" value=""/>
+            </td>
+            <td>
+                <span id="telSalleInfo"></span>
+            </td>
+        </tr>
+        
+        <!-- Fin champs pour modifier le numéro de téléphone de la salle -->
+        
+        <!-- Début champs pour modifier le nom du gérant -->     
+        
+        <tr>  
+	        <td> 
+            <label class="control-label" for="nomGerant">Nom du gérant de la salle :</label>
+            </td>
+            <td>      
+                <input class="form-control" id="nomGerant" type="text" name="nomGerant" placeholder=""/>
+            </td>
+            <td>
+                <span id="nomGerantInfo"></span>
+            </td>
+        </tr>
+        
+        <!-- Fin champs pour modifier le nom du gérant -->
+        
+        <!-- Début champs pour modifier le prénom du gérant -->      
+        
+		<tr>
+			<td>
+            <label class="control-label" for="nomGerant">Prénom du gérant de la salle :</label>
+			</td>
+			<td>
+                <input class="form-control" id="prenomGerant" type="text" name="prenomGerant" placeholder="" value=""/>
+            </td>
+            <td>
+                <span id="prenomGerantInfo"></span>
+            </td>
+        </tr>
+        
+        <!-- Fin champs pour modifier le prénom du gérant -->
+        
+        <!-- Début champs pour modifier 'comment contacter le gérant de la salle' --> 
+ 
+        <tr>
+	       	<td>
+            <label class="control-label" for="contactGerant">Comment contacter le gérant de la salle :</label>
+            </td>
+            <td>
+                <input class="form-control" id="contactGerant" type="text" name="contactGerant" placeholder="" value=""/>
+            </td>
+            <td>
+                <span id="contactGerantInfo"></span>
+            </td>
+        </tr>
+        
+        <!-- Fin champs pour modifier 'comment contacter le gérant de la salle' -->
+		
+		
+        <tr>
+	        <td>
+		        <a class="btn btn-default" href="f_mes_profils.php">Retour à mes profils</a>
+                
+            </td>
+            <td>
+			<input id="envoyer" class="btn btn-default" type="submit" value="Valider"/>
+            </td>
+        </tr>
+		
+            		</tbody>
+				</table>
+		   	</div>
+	    </div>
     </form>  
 </div>
 
