b544f6a13d05e4716c09528c0bea34bad0f5bc73
remiboissise@gmail.com <remiboissise@gmail.com@df39fe93-acf2-2b65-9ade-a2c068ddeaf1>
2015-03-14 17:12:45 +0000

    Modification JS (Peut prendre maintenant des chiffres pour le nom de la salle ou artiste)

--- trunk/web/js/verificationArtiste.js
+++ trunk/web/js/verificationArtiste.js
@@ -10,23 +10,24 @@ $(document).ready(function(){
 
 	
 	nomArtiste.blur(validateNomArtiste);
-	descriptionArtiste.blur(validateDescription);
+	descriptionArtiste.blur(validateDescriptionArtiste);
 		
 	nomArtiste.keyup(validateNomArtiste);
-	descriptionArtiste.keyup(validateDescription);
+	descriptionArtiste.keyup(validateDescriptionArtiste);
 
 	
 	form.submit(function(){
-		if(validateNomArtiste()){
+		if(validateNomArtiste() & validateDescriptionArtiste()){
+			alert("Bien enregistré");
 			return true;
 		} else {
-			alert("Veuillez entrer un nom d'artiste");
+			alert("Veuillez entrer un nom d'artiste ou vérifier le nombre de caractères de votre description");
 			return false;
 		}
 	});
 
 	function validateNomArtiste(){
-		if(!nomArtiste.val().match(/^[A-Za-zàéèêëîïôöûüùç.]+([ -][A-Za-zàéèêëîïôöûüùç.]{1,})*$/i)){
+		if(!nomArtiste.val().match(/^[A-Za-zàéèêëîïôöûüùç.0-9]+([ -0-9][A-Za-zàéèêëîïôöûüùç.0-9]{1,})*$/i)){
 			nomArtisteInfo.removeClass("glyphicon glyphicon-ok");
 			nomArtisteInfo.addClass("glyphicon glyphicon-remove");
 			return false;
@@ -37,25 +38,22 @@ $(document).ready(function(){
 		}
 	}
 	
-	function validateDescription(){
+	function validateDescriptionArtiste(){
 		
 	var nombreCaractere = $(descriptionArtiste).val().length;
     var nombreCaractere = 300 - nombreCaractere;
-    var nombreMots = jQuery.trim($(this).val()).split(' ').length;
     
-    if($(this).val() === '') {
-     	nombreMots = 0;
-    }
-    
-    var msg = nombreCaractere + ' Caractere(s) restant';
+    var msg = nombreCaractere + ' caractère(s) restant(s)';
 	descriptionArtisteInfo.text(msg);
 
 
     if (nombreCaractere < 0) { 
 	    descriptionArtisteInfo.css('color', 'red');
+	    return false;
 	} else { 
 	    descriptionArtisteInfo.css('color', 'black');  
-	     }
+	    return true; 
+	}
 	}
 		
 });
