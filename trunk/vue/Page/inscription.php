8ecc6411589929f182a4efb76a91e2580ef57577
remiboissise@gmail.com <remiboissise@gmail.com@df39fe93-acf2-2b65-9ade-a2c068ddeaf1>
2015-03-14 17:22:49 +0000

    Page inscritpion sous forme de tableau

--- trunk/vue/Page/inscription.php
+++ trunk/vue/Page/inscription.php
@@ -1,42 +1,44 @@
 <!-- vue/page -->
 
-<div class="container col-lg-12" id="formulaireI">
+<div class="col-lg-12" id="formulaireI">
     <h1> Formulaire d'inscription </h1>
-    <div class="row">
-    <form class="form-horizontal" id="formulaireInscription" method ="post" name="verificationInscription1">
+    	<div class="row">
+			<form class="form-horizontal" id="formulaireInscription" method ="post" name="verificationInscription1">
+				<table class="table">
+					<tbody>
+					
 
 <!-- Début champs pour entrer son email -->
 
 	
-		<div class="col-xs-1"></div>
-	    <div class="form-group">
-	            <label class="control-label col-xs-2" for="email">Votre mail : *</label> 
-	            	<div class="col-xs-6">
-	            		<input class="form-control" id="email" type="text" name="email" placeholder="test@sorbonne.fr" value=""/>
-	            	</div>		
-							<div class="col-xs-1">	
-								<span id="emailInfo"></span>
-							</div>
-								<div class="col-lg-2"></div>
-		</div>
+		<tr>
+	    	<td>
+	            <label class="control-label" for="email">Votre mail : *</label> 
+	    	</td>
+	    	<td>
+	            <input class="form-control" id="email" type="text" name="email" placeholder="test@sorbonne.fr" value=""/>
+	    	</td>
+	    	<td>				
+				<span id="emailInfo"></span>
+	    	</td>
+		</tr>
 	
 	
 <!-- Fin champs pour entrer son email -->
 
 <!-- Début champs pour entrer son pseudo -->
 
-	
-        <div class="col-xs-1"></div>
-		<div class="form-group">   
-            	<label class="control-label col-xs-2" for="pseudo">Pseudo : *</label>
-            		<div class="col-xs-6">
-						<input class="form-control" id="pseudo" type="text" name="pseudo" placeholder="lhommedu13" value=""/>
-            		</div>
-            				<div class="col-xs-1">
-								<span id="pseudoInfo"></span>
-            				</div>
-            					<div class="col-lg-2"></div>
-		</div>
+        <tr>
+			<td>  
+            	<label class="control-label" for="pseudo">Pseudo : *</label>
+			</td>
+			<td>
+				<input class="form-control" id="pseudo" type="text" name="pseudo" placeholder="lhommedu13" value=""/>
+			</td>
+			<td>
+            	<span id="pseudoInfo"></span>
+			</td>
+		</tr>
    
 
 <!-- Fin champs pour entrer son pseudo -->
@@ -44,46 +46,51 @@
 <!-- Début champs pour entrer son mot de passe -->
 
 	
-        <div class="col-xs-1"></div>
-		<div class="form-group">   
-            	<label class="control-label col-xs-2" for="passe">Mot de passe : *</label>
-            		<div class="col-xs-6">
-						<input class="form-control" id="passe" type="password" name="passe" value=""/>
-            		</div>
-            				<div class="col-xs-1">
-								<span id="passeInfo"></span>
-            				</div>
-            					<div class="col-lg-2"></div>
-		</div>
+        <tr>
+			<td>   
+            	<label class="control-label" for="passe">Mot de passe : *</label>
+			</td>
+			<td>
+				<input class="form-control" id="passe" type="password" name="passe" value=""/>
+			</td>
+			<td>
+				<span id="passeInfo"></span>
+			</td>
+		</tr>
     
         
 <!-- Fin champs pour entrer son mot de passe -->
 
 <!-- Début champs pour vérifier son mot de passe -->
 
-	
-        <div class="col-xs-1"></div>
-		<div class="form-group">   
-            	<label class="control-label col-xs-2" for="passe2">V&eacute;rification de votre mot de passe : *</label>
-            		<div class="col-xs-6">
-						<input class="form-control" id="passe2" type="password" name="passe2" value=""/>
-            		</div>
-            				<div class="col-xs-1">
-								<span id="passe2Info"></span>
-            				</div>
-            					<div class="col-lg-2"></div>
-		</div>
+		<tr> 
+			<td>
+            	<label class="control-label" for="passe2">V&eacute;rification de votre mot de passe : *</label>
+			</td>
+			<td>
+				<input class="form-control" id="passe2" type="password" name="passe2" value=""/>
+			</td>
+			<td>
+				<span id="passe2Info"></span>
+			</td>
+		</tr>
    
     
 <!-- Fin champs pour vérifier son mot de passe -->
 
-    <div>
-        <input class="btn btn-default" type="submit" value="M'inscrire" id="envoyer"/>
-    </div>
+    <tr>
+	    <td>
+        <input class="btn btn-default center-block" type="submit" value="M'inscrire" id="envoyer"/>
+	    </td>
+    </tr>
     
-    </form>
+    
+				</tbody>
+			</table>
+    	</form>
     </div>
 </div>
+
 
 
 <div class="container col-lg-6">
