a3151edd8b7c44182f43cf927fa8c52a70bb700e
remiboissise@gmail.com <remiboissise@gmail.com@df39fe93-acf2-2b65-9ade-a2c068ddeaf1>
2015-03-16 12:12:05 +0000

    Vérification titre de la musique

---
+++ trunk/web/js/verificationMusique.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	
+	var form = $('#formulaireAjoutMusique');
+	
+	var nomMusique = $('#titre');
+
+	
+	form.submit(function(){
+		if(validateNomMusique()){
+			alert("Bien ajouté");
+			return true;
+		} else {
+			alert("Veuillez entrer un titre");
+			return false;
+		}
+	});
+
+	function validateNomMusique(){
+		if(!nomMusique.val().match(/^[A-Za-zàéèêëîïôöûüùç.0-9]+([ -0-9][A-Za-zàéèêëîïôöûüùç.0-9]{1,})*$/i)){
+			return false;
+		} else {
+			return true;
+		}
+	}
+});
+
