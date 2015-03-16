4a293a3a8b501ae7dcd49cc0d739a3280e646e57
remiboissise <remiboissise@gmail.com>
2015-03-16 08:30:11 +0000

    Changement du nom du site

--- vue/template/head.php
+++ vue/template/head.php
@@ -2,7 +2,7 @@
 
 <html>
     <head>
-        <title>MATCH'UP_INSCRIPTION</title>
+        <title>MATCH'UP</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="web/css/bootstrap.css" />
