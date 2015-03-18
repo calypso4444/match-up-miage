7e049909850433480f1fdce49cb4501223e183cb
remiboissise@gmail.com <remiboissise@gmail.com@df39fe93-acf2-2b65-9ade-a2c068ddeaf1>
2015-03-18 11:34:15 +0000

    Changement du menu de connexion test1

--- trunk/vue/template/header.php
+++ trunk/vue/template/header.php
@@ -2,11 +2,11 @@
     <div class="row-same-height">
         <div class="col-lg-9 col-sm-height">
             <div id="banner">
-                <a href="index.php"><img src="web/image/banner_nobackground.png" alt="banner"/></a>
+                <a href="index.php"><img src="web/image/banner_nobackground.png" alt="banner" /></a>
             </div>
             <?php include_once 'nav.php';?>
         </div>
-        <div id='menuConnexion' class="col-lg-2 col-sm-height">
+        <div id='menuConnexion' class="col-lg-2 col-sm-height" style="display:flex; height: auto ; vertical-align: top;">
             <?php if (isset($_SESSION['user'])) : ?>
             	<a href="f_info_perso.php"><img src="<?php echo $_SESSION['user']['avatar']; ?>" alt="avatar" class="img img-responsive"/></a></br>
                 <a href="f_mes_profils.php" class="glyphicon glyphicon-user"> mes profils </a></br>
@@ -15,11 +15,18 @@
                 <a href="f_mes_participations.php" class="glyphicon glyphicon-thumbs-up"> mes participations </a></br>
                 <a href="deconnexion.php" class="glyphicon glyphicon-off"> deconnexion </a>
             <?php else: ?>
-                <p>Connexion :</p>
+                	                <h3 class="form-signin-heading">Connexion</h3>
                 <form action="connexion.php" method="POST">
-                    <input type="text" name="login" placeholder='Votre login' /></br>
-                    <input type="password" name="passe" placeholder='Votre mot de passe' /></br>
-                    <input type="submit" value="OK" />
+                    <div style="margin-bottom: 5px" class="input-group">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                        <input id="login-username" type="text" class="form-control" name="login" value="" placeholder="pseudo / email">                                      	                </div>
+                    <div style="margin-bottom: 5px" class="input-group">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                        <input id="login-password" type="password" class="form-control" name="passe" placeholder="mot de passe">
+                    </div>
+                    <div class="text-right">
+                    <input class="btn btn-success" type="submit" value="OK" />
+                    </div>
                 </form>
 
                 <a href="inscription.php">Pas encore inscrit?</a></br>
