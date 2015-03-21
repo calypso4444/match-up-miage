88a59bbb33c5f681dec23a822e88ac1e6314b26c
remiboissise@gmail.com <remiboissise@gmail.com@df39fe93-acf2-2b65-9ade-a2c068ddeaf1>
2015-03-21 15:48:06 +0000

    Js salleAdmin

---
+++ trunk/web/js/salleAdmin.js
@@ -0,0 +1,20 @@
+/* Cette fonction va permettre de masquer au chargement de la page le contenu de l'admin */
+document.getElementById("cacher").onload = function() {masquer("salleAdmin")};	
+function masquer(id){
+	var estProprietaire = $('#estProprietaire').val();
+	  if(estProprietaire == 1 ) {
+	    document.getElementById(id).style.display = 'block';
+	  } else {
+		document.getElementById(id).style.display = 'none';	  
+	  }
+}
+
+/* Cette fonction va permettre de masquer au clique de la page le contenu de l'admin */
+function masquer_div(id){
+	var estProprietaire = $('#estProprietaire').val();
+	  if((document.getElementById(id).style.display == 'none') && estProprietaire == 1 ) {
+	       document.getElementById(id).style.display = 'block';
+	  } else {
+	       document.getElementById(id).style.display = 'none';
+	  }
+}	
