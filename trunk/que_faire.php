68728d36b54df128397027456a1ee8dcdb2de33f
lajungglespeedienne@gmail.com <lajungglespeedienne@gmail.com@df39fe93-acf2-2b65-9ade-a2c068ddeaf1>
2015-03-21 20:29:25 +0000

    bug reparé sur la page que_faire.php (participer) redirection vers la page d'inscription si l'utilisateur n'est pas connecte ajout de commentaires

--- trunk/que_faire.php
+++ trunk/que_faire.php
@@ -9,17 +9,27 @@ include_once 'config/includeGlobal.php';
 
 /* séquence du controleur */
 
+//on initialise une variable qui permettra d'indiquer si l'utilisateur est connecté, il pourra interagir avec le systeme si oui, si non il sera redirigé  
+$estConnecte = '';
+
+//recupere tous les concerts qui se deroulent à partir de la date courante
 $concert = $model['GestionnaireConcert']->getAllConcert();
 
+//si un utilisateur essaye participer a un concert, si il est connecte le concert est ajouté a ses participations sinon il est redirigé vers la page d'accueil
 $participation = filter_input(INPUT_GET, 'nConcert');
 if (isset($participation)) {
-    if (isset($id)) {
+    $id = $_SESSION['user']['id'];
+    if (!isset($id)) {
+        $estConnecte = false;
+    } else {
+        $estConnecte = true;
         $model['GestionnaireUtilisateur']->participer($id, $participation);
     }
 }
 
-$date= new DateTime();
-$date=$date->format('20y-m-d');
+//permet l'affichage sur la carte des concerts de la date courante
+$date = new DateTime();
+$date = $date->format('20y-m-d');
 $concertCarte = $model['GestionnaireCarte']->getAllSalleConcertByDate($date);
 
 /* fin de séquence */
@@ -27,6 +37,7 @@ $concertCarte = $model['GestionnaireCarte']->getAllSalleConcertByDate($date);
 /* affichage de la vue */
 
 $vue = array();
+$vue['estConnecte'] = $estConnecte;
 $vue['concert'] = $concert;
 $vue['concertCarte'] = $concertCarte;
 $view->render('que_faire', $vue);
