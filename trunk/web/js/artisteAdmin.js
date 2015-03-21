099771c5d81297714c0deb50cc5ec35902a8688e
remiboissise@gmail.com <remiboissise@gmail.com@df39fe93-acf2-2b65-9ade-a2c068ddeaf1>
2015-03-21 15:47:54 +0000

    Js artisteADmin

---
+++ trunk/web/js/artisteAdmin.js
@@ -0,0 +1,23 @@
+/* Cette fonction va permettre de masquer au chargement de la page le contenu de l'admin */
+document.getElementById("cacher").onload = function() {masquer("artisteAdmin")};	
+function masquer(id){
+	var estProprietaire = $('#estProprietaire').val();
+	  if(estProprietaire == 1 ) {
+	    document.getElementById(id).style.display = 'block';
+	  } else {
+		document.getElementById(id).style.display = 'none';	  
+	  }
+}
+
+/* Cette fonction va permettre de masquer au clique de la page le contenu de l'admin */
+function masquer_div(id){
+	var estProprietaire = $('#estProprietaire').val();
+	  if((document.getElementById(id).style.display == 'none') && estProprietaire == 1 ) {
+	       document.getElementById(id).style.display = 'block';
+	  } else {
+	       document.getElementById(id).style.display = 'none';
+	  }
+}	
+
+
+	
