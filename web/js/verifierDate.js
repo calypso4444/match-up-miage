225d0babf9d832c31d05dc6552b8108f804a69b0
remiboissise <remiboissise@gmail.com>
2015-03-22 11:04:27 +0000

    Calendrier fonctionne

--- web/js/verifierDate.js
+++ web/js/verifierDate.js
@@ -1,17 +1,8 @@
 $(document).ready(function() {
     $('#datepicker').datepicker({
-        onSelect: function(dateText, inst) {
-            //Get today's date at midnight
-            var today = new Date();
-            today = Date.parse(today.getMonth()+1+'/'+today.getDate()+'/'+today.getFullYear());
-            //Get the selected date (also at midnight)
-            var selDate = Date.parse(dateText);
-
-            if(selDate < today) {
-                //If the selected date was before today, continue to show the datepicker
-                $('#datepicker').val('');
-                $(inst).datepicker('show');
-            }
-        }
-    });
+	    dateFormat : 'dd/mm/yy',
+	    minDate : 0
+		
+				
+     });
 });
