d5dbc1770e83937c5b40f55217126e6dbf2a153d
lajungglespeedienne <lajungglespeedienne@gmail.com>
2015-03-22 09:34:37 +0000

    suppression profil artiste avec demande de confirmation ok

--- supprimer_artiste.php
+++ supprimer_artiste.php
@@ -6,12 +6,24 @@ include_once 'config/includeGlobal.php';
 
 /* fin de l'instanciation */
 
-/* séquence du controleur */ 
-$nArtiste = filter_input(INPUT_GET, 'nArtiste');
+/* séquence du controleur */
 
 $id = $_SESSION['user']['id'];
-$model['GestionnaireProfil']->supprimerArtiste($nArtiste, $id);
- 
+
+if (isset($id)) {
+    $nArtiste = filter_input(INPUT_GET, 'nArtiste');
+    if (isset($nArtiste)) {
+        $confirm = filter_input(INPUT_GET, 'confirm');
+        if (isset($confirm)) {
+            if ($confirm === "true") {
+                $model['GestionnaireProfil']->supprimerArtiste($nArtiste, $id);
+            }
+        } else {
+            echo "<script>var tmp=confirm('Voulez vous vraiment supprimer ce profil ?'); if(tmp){document.location.href='supprimer_artiste.php?nArtiste=" . $nArtiste . "&confirm=true';}</script>";
+        }
+    }
+}
+
 /* fin de séquence */
 
 /* affichage de la vue */
--- supprimer_salle.php
+++ supprimer_salle.php
@@ -1,16 +1,30 @@
 <?php
+
 /* instanciation des fichiers de config + modele */
 
 include_once 'config/includeGlobal.php';
 
 /* fin de l'instanciation */
 
-/* séquence du controleur */ 
-$nSalle = filter_input(INPUT_GET, 'nSalle');
+/* séquence du controleur */
 
 $id = $_SESSION['user']['id'];
-$model['GestionnaireProfil']->supprimerSalle($nSalle, $id);
- 
+
+if (isset($id)) {
+    $nSalle = filter_input(INPUT_GET, 'nSalle');
+    if (isset($nSalle)) {
+        $confirm = filter_input(INPUT_GET, 'confirm');
+        if (isset($confirm)) {
+            if ($confirm === "true") {
+                $model['GestionnaireProfil']->supprimerSalle($nSalle, $id);
+            }
+        } else {
+            echo "<script>var tmp=confirm('Voulez vous vraiment supprimer ce profil ?'); if(tmp){document.location.href='supprimer_salle.php?nSalle=" . $nSalle . "&confirm=true';}</script>";
+        }
+    }
+}
+
+
 /* fin de séquence */
 
 /* affichage de la vue */
