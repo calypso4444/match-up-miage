4285077f03a2d2f9ed8634710e0334c3cff33f39
lajungglespeedienne@gmail.com <lajungglespeedienne@gmail.com@df39fe93-acf2-2b65-9ade-a2c068ddeaf1>
2015-03-23 19:01:50 +0000

    desinscription ok

---
+++ trunk/desinscription.php
@@ -0,0 +1,31 @@
+<?php
+
+/* instanciation des fichiers de config + modele */
+
+include_once 'config/includeGlobal.php';
+
+/* fin de l'instanciation */
+
+/* séquence du controleur */
+
+    $confirm = false;
+    $id = filter_input(INPUT_GET, 'id');
+    if (isset($id)) {
+        $confirm = filter_input(INPUT_GET, 'confirm');
+        if (isset($confirm)) {
+            if ($confirm === "true") {
+                $model['GestionnaireUtilisateur']->desinscription($id);
+                $_SESSION['user'] = null;
+                echo "<script>document.location.href='indexdesinscription.php</script>";
+            }
+        } else {
+            echo "<script>var tmp=confirm('Voulez vous vraiment vous desinscrire ?'); if(tmp){document.location.href='desinscription.php?id=" . $id . "&confirm=true';}</script>";
+        }
+    }
+
+/* fin de séquence */
+
+/* redirection */
+
+include 'index.php';
+?>
