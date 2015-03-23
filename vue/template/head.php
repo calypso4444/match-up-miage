935c59474126584edd29e62399ad94d99fb2cc8f
remiboissise <remiboissise@gmail.com>
2015-03-23 12:25:36 +0000

    Ajout du script pour la photo

--- vue/template/head.php
+++ vue/template/head.php
@@ -26,6 +26,7 @@
         <script type="text/javascript" src="web/js/jquery-ui.min.js"></script>
         <script type="text/javascript" src="web/js/verificationAnnonceArtiste.js"></script>
         <script type="text/javascript" src="web/js/verificationAnnonceSalle.js"></script>
+		<script type="text/javascript" src="web/js/zoomImageAlbum.js"></script>
 		
     </head>
 <body>
