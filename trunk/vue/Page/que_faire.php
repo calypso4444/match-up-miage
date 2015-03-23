fb617a8c9feffaef6c09af2b631e4e32dac58b08
lajungglespeedienne@gmail.com <lajungglespeedienne@gmail.com@df39fe93-acf2-2b65-9ade-a2c068ddeaf1>
2015-03-23 00:37:20 +0000

    filtre arrondissement et genre musical ok pour la page que faire

--- trunk/vue/Page/que_faire.php
+++ trunk/vue/Page/que_faire.php
@@ -16,12 +16,63 @@ if ($vars['estConnecte'] === false) {
     <p class='text-center'>Concerts du <?php echo $vars['dateConcert'] . " " . $vars['nomMois']; ?>
 
     <div id='filtreConcert' class='col-lg-2' style='border-top:black 2px solid'>
+        <h4>Affiner votre recherche</h4>
        	Choisissez un jour pour lequel vous voulez un affichage sur la carte</p>
         <form action="que_faire.php" method="post" name='choixJour'>
             <input id="datepicker" class="datepicker form-control" name="choixJour"></input>
             </br>
             <input type='submit' class='btn-success' value="Valider"/>
         </form>
+
+        <form action='que_faire.php' method="post" name="formRechercheAvanceGenre" >
+            <table class="table">
+                <tbody>
+                    <tr> <td> 
+                            <p>Par genre musical</p>
+                            <select class='form-control' name="genreMusical" size="l">
+                                <option></option>
+                                <option> R&B </option>
+                                <option> Punk </option>
+                                <option> Classique</option>
+                                <option> Metal</option>
+                                <option> Pop</option>
+                                <option> Rap</option>
+                                <option> Reggae</option>
+                                <option> Dance </option>          
+                            </select>
+                        </td></tr>
+                    <tr><td>
+                            <p>Par arrondissement</p>
+                            <select class='form-control' name="arrondissement" size="l">
+                                <option></option>
+                                <option> 01er Arrondissement </option>
+                                <option> 02e Arrondissement </option>
+                                <option> 03e Arrondissement</option>
+                                <option> 04e Arrondissement</option>
+                                <option> 05e Arrondissement</option>
+                                <option> 06e Arrondissement</option>
+                                <option> 07e Arrondissement</option>
+                                <option> 08e Arrondissement </option>          
+                                <option> 09e Arrondissement </option>
+                                <option> 10e Arrondissement </option>
+                                <option> 11e Arrondissement </option>
+                                <option> 12e Arrondissement </option>
+                                <option> 13e Arrondissement </option>
+                                <option> 14e Arrondissement </option>
+                                <option> 15e Arrondissement </option>
+                                <option> 16e Arrondissement </option>
+                                <option> 17e Arrondissement </option>
+                                <option> 18e Arrondissement </option>
+                                <option> 19e Arrondissement </option>
+                                <option> 20e Arrondissement </option>
+                            </select>
+                        </td></tr>
+                </tbody>
+            </table>
+            <input type='submit' class="btn-success"></input>
+        </form>
+        </br>
+
     </div>
 
     <section id='carte'class="text-center col-lg-10" style='border-left:black 2px solid; border-top:black 2px solid '>
