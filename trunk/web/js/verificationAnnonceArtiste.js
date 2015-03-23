1a7d4cf5c99a013bf70c4cad213d07a8bf54905a
remiboissise@gmail.com <remiboissise@gmail.com@df39fe93-acf2-2b65-9ade-a2c068ddeaf1>
2015-03-23 17:14:09 +0000

    Commentaire JS vérificationAnnonceArtiste

--- trunk/web/js/verificationAnnonceArtiste.js
+++ trunk/web/js/verificationAnnonceArtiste.js
@@ -1,12 +1,25 @@
+/* Ce JS a pour but de vérifier le nombre de caractère d'une annonce */
+
 $(document).ready(function(){
+	
+	/* On va récupérer l'ensemble des attributs  */
 	
 	var formAnnonce = $('#posterAnnonce');
 	var descriptionAnnonceArtiste = $('#descriptionAnnonceArtiste');
 	var descriptionArtisteInfo = $('#descriptionArtisteInfo');
 
+	/* La fonction blur permet de déclencher l'évènement qui se produit lorsque l'élément perd le focus */
+	
 	descriptionAnnonceArtiste.blur(validateAnnonceArtiste);
+	
+	/* La fonction keyup permet de déclencher l'évènement qui se produit lorsque l'on tape sur une touche */
+	
 	descriptionAnnonceArtiste.keyup(validateAnnonceArtiste);
 
+	/* Lors de la validation, on verrifie la valeur de retour de la fonction validateAnnonceArtiste. Si la valeur est fausse,
+	alors l'utilisateur devra rectifier son annonce.
+	*/
+	
 	formAnnonce.submit(function(){
 		if(validateAnnonceArtiste()){
 			return true;
@@ -16,6 +29,7 @@ $(document).ready(function(){
 		}
 	});
 	
+	/* Cette fonction a pour but de vérifier le nombre de caractères de l'annonce */
 	function validateAnnonceArtiste(){
 		
 	var nombreCaractere = $(descriptionAnnonceArtiste).val().length;
