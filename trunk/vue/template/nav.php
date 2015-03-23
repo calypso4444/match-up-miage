39801540db7791f61fc06e8f992eed8bae3e594b
remiboissise@gmail.com <remiboissise@gmail.com@df39fe93-acf2-2b65-9ade-a2c068ddeaf1>
2015-03-23 16:28:40 +0000

    Suppression d'id inutile et de la balise style

--- trunk/vue/template/nav.php
+++ trunk/vue/template/nav.php
@@ -1,18 +1,11 @@
-<nav>
-	
-	<style>
-
-		
-		</style>
-	
+<nav>	
     </br>
     <ul class="menu navbar nav-justified navbar-form ">
-
-        <li id="border" class="text-center active"><a id="navHover" href="index.php">accueil</a></li>
-        <li id="border" class="text-center active"><a id="navHover" href="que_faire.php">que faire ?</a></li>
-        <li id="border" class="text-center active"><a id="navHover" href="tous_les_artistes.php">artistes</a></li>
-        <li id="border" class="text-center active"><a id="navHover" href="toutes_les_salles.php">salles</a></li>
-        <li id="border" class="text-center active"><a id="navHover" href="petites_annonces.php">petites annonces</a></li>		
+        <li id="border" class="text-center active"><a href="index.php">accueil</a></li>
+        <li id="border" class="text-center active"><a href="que_faire.php">que faire ?</a></li>
+        <li id="border" class="text-center active"><a href="tous_les_artistes.php">artistes</a></li>
+        <li id="border" class="text-center active"><a href="toutes_les_salles.php">salles</a></li>
+        <li id="border" class="text-center active"><a href="petites_annonces.php">petites annonces</a></li>		
 
         <li class="text-center">
             <form id="formulaireRecherche" method ="post" name="recherche" action='f_recherche_avancee.php'>
@@ -22,7 +15,6 @@
             </form>
         </li>
     </ul>
-
 </nav>
 
 
